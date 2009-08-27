06df17a44ad8760e494c3703d61f2c777e7f4da6
Andrew Brown <andrew@monsterboxpro.com>
2009-08-26 20:03:29 -0400

    Setup the basic template, Styled login and signup page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,29 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def header
+    render :partial => 'shared/header'
+  end
+
+  def project_navigation
+    render :partial => 'shared/project_navigation',
+      :locals => { :project => project }
+  end
+  
+  def navigation
+    render :partial => 'shared/navigation'
+  end
+
+  def footer
+    render :partial => 'shared/footer'
+  end
+
+  def location_name?(names)
+    names.any?{ |name| name == location_name }
+  end
+    
+  def location_name
+    "#{action_name}_#{controller.controller_name}"
+  end
+    
 end
