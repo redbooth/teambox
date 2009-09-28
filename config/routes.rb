6f308846bd721e091a622fb859f1401905fa25de
Pablo Villalba <pablo@saiku.es>
2009-09-28 19:57:43 +0200

    Preliminary support to drag task lists in the sidebar

--- config/routes.rb
+++ config/routes.rb
@@ -29,7 +29,7 @@ ActionController::Routing::Routes.draw do |map|
     end
     project.resources :uploads, :requirements => { :id => /[^\/]+/ }, :member => { :thumbnail => :get }
     
-    project.resources :task_lists, :has_many => [:comments] do |task_lists|
+    project.resources :task_lists, :has_many => [:comments], :member => { :reorder => :post } do |task_lists|
       task_lists.resources :tasks, :has_many => [:comments], :member => { :check => :put, :uncheck => :put }
     end
     
