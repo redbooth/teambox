f11c4130626ec02d19e9037f4832ea2effad8987
Pablo Villalba <pablo@saiku.es>
2009-10-23 17:06:47 +0200

    Since login token needs to be a shorter string, we will not add the datetime after the hex digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -87,17 +87,19 @@ class User < ActiveRecord::Base
   end
   
   def activate!
-    self.update_attribute :confirmed_user, true
+    self.confirmed_user = true
+    self.save
   end
   
   def generate_login_code!
-    self.login_token = Digest::SHA1.hexdigest(rand(999999999).to_s) + Time.new.to_i.to_s
+    self.login_token = Digest::SHA1.hexdigest(rand(999999999).to_s)
     self.login_token_expires_at = 1.month.from_now
     self.save
   end
   
   def expire_login_code!
-    self.update_attribute :login_token_expires_at, 1.minute.ago
+    self.login_token_expires_at = 1.minute.ago
+    self.save
   end
   
   def is_login_token_valid?(token)
