e53ffdc5cf053652f4ac7900521c2f415087dc19
Pablo Villalba <pablo@saiku.es>
2009-10-26 12:02:22 +0100

    Now we only require 30 words to consider a biography complete

--- app/models/user/completeness.rb
+++ app/models/user/completeness.rb
@@ -2,9 +2,9 @@ class User
   
   define_completeness_scoring do
     check :biography, lambda { |per| per.biography.present? },    :biography_presence
-    check :biography, lambda { |per| per.biography.length > 30 }, :biograhpy_length_short
-    check :biography, lambda { |per| per.biography.length > 128 }, :biograhpy_length_average
-    check :biography, lambda { |per| per.biography.length > 250 }, :biograhpy_length_long
+    check :biography, lambda { |per| per.biography.length > 10 }, :biography_length_short
+    check :biography, lambda { |per| per.biography.length > 20 }, :biography_length_average
+    check :biography, lambda { |per| per.biography.length > 30 }, :biography_length_long
   end
   
   def profile_complete?
@@ -12,9 +12,9 @@ class User
   end
 
   def update_profile_score
-    self.profile_score = self.completeness_score
+    self.profile_score   = self.completeness_score
     self.profile_percent = self.percent_complete
-    self.profile_grade = self.completeness_grade.to_s
+    self.profile_grade   = self.completeness_grade.to_s
   end
 
 end
