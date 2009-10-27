4cce46958eefdb9e1c966eba85d19aef32c617d6
Pablo Villalba <pablo@saiku.es>
2009-10-27 08:04:20 +0100

    Emailer helpers to substitute css styles

---
+++ app/helpers/emailer_helper.rb
@@ -0,0 +1,17 @@
+module EmailerHelper
+  def email_global
+    'font-size: 14px; color: rgb(50,50,50); font-family: Helvetica, Arial'
+  end
+  
+  def email_box
+    'background-color: rgb(255,255,200); margin: 10px; padding: 10px; border: 1px rgb(220,220,150) solid'
+  end
+
+  def email_text(size)
+    case size
+    when :small then 'font-size: 10px; color: rgb(100,100,100)'
+    when :big   then 'font-size: 18px; color: rgb(0,0,0)'
+    end
+  end
+
+end
