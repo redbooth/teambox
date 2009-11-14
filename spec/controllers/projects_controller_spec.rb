6712f1557a96c95183834fae18b6c16d2e77466b
Andrew Brown <andrew@monsterboxpro.com>
2009-11-14 21:10:14 +0100

    Commenting prject spec

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,14 +1,14 @@
-require File.dirname(__FILE__) + '/../spec_helper'
+curequire File.dirname(__FILE__) + '/../spec_helper'
 
 describe ProjectsController do
-  describe "on GET to /projects/new" do
-    before(:each) do
-      Factory.stub(:project)
-      @user = Factory(:user)
-      controller.stubs(:current_user).returns(@user)
-      get :new
-    end
-
-    it "assigns the new project to the current user"
-  end 
+  #describe "on GET to /projects/new" do
+  #  before(:each) do
+  #    Factory.stub(:project)
+  #    @user = Factory(:user)
+  #    controller.stubs(:current_user).returns(@user)
+  #    get :new
+  #  end
+  #
+  #  it "assigns the new project to the current user"
+  #end 
 end
