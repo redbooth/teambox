9524b5c032ea62ba8ea00c227a9c83be6b3f17df
Pablo Villalba <pablo@saiku.es>
2009-11-15 22:50:18 +0100

    Updating domain_tld_regex so it will recognize domains like .cat

--- vendor/plugins/restful-authentication/lib/authentication.rb
+++ vendor/plugins/restful-authentication/lib/authentication.rb
@@ -14,7 +14,7 @@ module Authentication
 
   self.email_name_regex  = '[\w\.%\+\-]+'.freeze
   self.domain_head_regex = '(?:[A-Z0-9\-]+\.)+'.freeze
-  self.domain_tld_regex  = '(?:[A-Z]{2}|com|org|net|edu|gov|mil|biz|info|mobi|name|aero|jobs|museum)'.freeze
+  self.domain_tld_regex  = '(?:[A-Z]{2,3}|com|org|net|edu|gov|mil|biz|info|mobi|name|aero|jobs|coop|museum)'.freeze
   self.email_regex       = /\A#{email_name_regex}@#{domain_head_regex}#{domain_tld_regex}\z/i
   self.bad_email_message = "should look like an email address.".freeze
 
