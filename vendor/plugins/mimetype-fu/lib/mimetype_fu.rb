9b74ac0ecc46223ff490cc3b1499385ec3e9bd5f
Andrew Brown <andrew@monsterboxpro.com>
2009-12-04 18:05:53 +0100

    checks for file extension using a matcher instead of just checking for last used . with rindex

--- vendor/plugins/mimetype-fu/lib/mimetype_fu.rb
+++ vendor/plugins/mimetype-fu/lib/mimetype_fu.rb
@@ -7,23 +7,23 @@ class File
     case file
     when File, Tempfile
       unless RUBY_PLATFORM.include? 'mswin32'
-        mime = `file --mime -br "#{file.path}"`.strip
+        mime = `file --mime-type -br "#{file.path}"`.strip
       else
         mime = EXTENSIONS[File.extname(file.path).gsub('.','').downcase.to_sym]
       end
     when String
-      mime = EXTENSIONS[(file[file.rindex('.')+1, file.size]).downcase.to_sym] unless file.rindex('.').nil?
+        mime = EXTENSIONS[File.extname(file).delete('.').downcase.to_sym] unless file.match(/\.\w+$/).nil?
     when StringIO
       temp = File.open(Dir.tmpdir + '/upload_file.' + Process.pid.to_s, "wb")
       temp << file.string
       temp.close
-      mime = `file --mime -br "#{temp.path}"`
+      mime = `file --mime-type -br "#{temp.path}"`
       mime = mime.gsub(/^.*: */,"")
       mime = mime.gsub(/;.*$/,"")
       mime = mime.gsub(/,.*$/,"")
       File.delete(temp.path)
     end
-    return mime || 'unknown/unknown'
+    mime || 'unknown/unknown'
    end
 
   def self.extensions
