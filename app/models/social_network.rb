39248814986eb902d09720a7307b53834b9af34b
Andrew Brown <andrew@monsterboxpro.com>
2009-12-05 22:56:52 +0100

    fixed the passing wrong variable for social_networks

--- app/models/social_network.rb
+++ app/models/social_network.rb
@@ -2,4 +2,12 @@ class SocialNetwork < ActiveRecord::Base
   belongs_to :user
   NETWORK_TYPES = ['Twitter','Facebook','Linked In','FriendFeed','MySpace','Seesmic','Delicious','Stumble Upon','Other']
   TYPES = ['Personal','Business','Other']
+
+  def get_network_type
+    NETWORK_TYPES[account_type]
+  end
+  
+  def get_type
+    TYPES[account_type]
+  end
 end
