7fab7d3688652162a13bf92fddb0adc3469f554d
Andrew Brown <andrew@monsterboxpro.com>
2009-12-05 19:08:12 +0100

    refactored using find_by, fixed editable role

--- app/models/project/roles.rb
+++ app/models/project/roles.rb
@@ -5,22 +5,24 @@ class Project
   end
 
   def admin?(user)
-    if p = people.find(:first, :conditions => {:user_id => user.id})
+    if p = people.find_by_user_id(user.id)
       p.role == 3 || p.owner?
+    else
+      false  
     end
   end
   
   def observer?(user)
-    if p = people.find(:first, :conditions => {:user_id => user.id})
-      p.role == 0 && p.owner? == false
+    if p = people.find_by_user_id(user.id)
+      p.role == 0 && !p.owner?
     else
       false
     end
   end
   
   def editable?(user)
-    if p = people.find(:first, :conditions => {:user_id => user.id})
-      p.owner? || p.role > 0
+    if p = people.find_by_user_id(user.id)
+      p.owner? || p.role > 1
     else
       false
     end
