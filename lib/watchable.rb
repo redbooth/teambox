55729b38dd1a856e2f59f17c611a798c288dc55f
Pablo Villalba <pablo@saiku.es>
2009-12-06 22:25:55 +0100

    Watchers retrieving additional columns

--- lib/watchable.rb
+++ lib/watchable.rb
@@ -26,7 +26,7 @@ module Watchable
   
   def watchers
     self.watchers_ids.if_defined.collect do |id|
-      User.find id, :select => "id, email, first_name, last_name, language, :notify_conversations, :notify_task_lists, :notify_tasks"
+      User.find id, :select => "id, email, first_name, last_name, language, notify_conversations, notify_task_lists, notify_tasks"
     end
   end
 
