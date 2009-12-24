b406e9f88c1c44a5e69c04460874a1e69026853a
Pablo Villalba <pablo@saiku.es>
2009-12-24 13:38:25 +0100

    Editing haml

--- vendor/plugins/haml/init.rb
+++ vendor/plugins/haml/init.rb
@@ -1,16 +1,8 @@
 begin
   require File.join(File.dirname(__FILE__), 'lib', 'haml') # From here
 rescue LoadError
-  begin
-    require 'haml' # From gem
-  rescue LoadError => e
-    # gems:install may be run to install Haml with the skeleton plugin
-    # but not the gem itself installed.
-    # Don't die if this is the case.
-    raise e unless defined?(Rake) && Rake.application.top_level_tasks.include?('gems:install')
-  end
+  require 'haml' # From gem
 end
 
-# Load Haml and Sass.
-# Haml may be undefined if we're running gems:install.
-Haml.init_rails(binding) if defined?(Haml)
+# Load Haml and Sass
+Haml.init_rails(binding)
