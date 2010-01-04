f4a801e20f5c85e404c0ff670a4c388bc078fc2a
Alejandro Julián López <alejandrojl@me.com>
2010-01-05 00:35:47 +0100

    Added links to socials networks from Card

--- app/models/social_network.rb
+++ app/models/social_network.rb
@@ -10,4 +10,21 @@ class SocialNetwork < ActiveRecord::Base
   def get_type
     TYPES[account_type]
   end
+  
+  def to_url
+    if name.include? ".com/"
+      name
+    else
+      case get_network_type
+        when 'Twitter'      then "http://www.twitter.com/#{name}"
+        when 'Facebook'     then "http://www.facebook.com/#{name}"
+        when 'Linked In'    then "http://www.us.linkedin.com/in/#{name}"
+        when 'FriendFeed'   then "http://www.friendfeed.com/#{name}"
+        when 'MySpace'      then "http://www.myspace.com/#{name}"
+        when 'Seesmic'      then "http://www.seesmic.tv/#{name}"
+        when 'Delicius'     then "http://www.delicious.com/#{name}"
+        when 'Stumble Upon' then "http://#{name}.stumbleupon.com/"
+      end
+    end
+  end
 end
--- app/models/website.rb
+++ app/models/website.rb
@@ -6,4 +6,12 @@ class Website < ActiveRecord::Base
   def get_type
     TYPES[account_type]
   end
+  
+  def to_url
+      if name.include? "://"
+          name
+      else
+          "http://#{name}"
+      end
+  end
 end
