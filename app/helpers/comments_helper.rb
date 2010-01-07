redbooth/teambox

commit c288bb309b9f9ec4589256228ed45e63260f01b4

refactored list_comments to include wrapper div

app/helpers/comments_helper.rb
@@ -98,7 +98,13 @@ module CommentsHelper
   end
   
   def list_comments(comments,target)
-    render :partial => 'comments/list_comments', :locals => { :comments => comments, :target => target }
+    content_tag :div,
+    render(:partial => 'comments/list_comments', 
+      :locals => { 
+        :comments => comments, 
+        :target => target }),
+      :class => 'comments',
+      :id => 'comment'
   end
   
   def show_comment(comment)

app/views/conversations/show.haml
@@ -3,8 +3,6 @@
 = conversation_form(@current_project,@conversation)
 = people_watching(@current_project,current_user,@conversation)
 = new_comment_form(@current_project,@current_project.comments.new,:target => @conversation)
-
-#comments
-  = list_comments(@comments, @conversation)
+= list_comments(@comments, @conversation)
   
 

app/views/hours/index.haml
@@ -8,6 +8,5 @@
     =list_hour_users(@users_displayed)
   =observe_user_filter
  
-= new_hour_comment_form(@current_project) 
-#comments
-  =list_comments(@comments,@current_project)
+= new_hour_comment_form(@current_project)
+= list_comments(@comments,@current_project)

app/views/task_lists/_show.haml
@@ -4,6 +4,4 @@
   = tabular_task_list(project,task_list,'all')
   = people_watching(project,current_user,task_list)
   = new_comment_form(project,project.comments.new, :target => task_list)
-
-  #comments
-    = list_comments(task_list.comments,task_list)
+  = list_comments(task_list.comments,task_list)

app/views/tasks/_show.haml
@@ -4,5 +4,4 @@
   = people_watching(project,current_user,task)
   = new_comment_form(project,comment, :target => task) unless task.closed?
   = task_archive_box(project,task_list,task)
-  #comments
-    = list_comments(task.comments,task)
+  = list_comments(task.comments,task)

commit c3c0ff1b62849be8ac5166d71b998dc1863835d9

Commit c288bb30 broke comments for tasks. Solving

app/helpers/comments_helper.rb
@@ -118,7 +118,7 @@ module CommentsHelper
         :comments => comments, 
         :target => target }),
       :class => 'comments',
-      :id => 'comment'
+      :id => 'comments'
   end
   
   def show_comment(comment)