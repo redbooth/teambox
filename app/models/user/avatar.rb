5d30f68e8259820335512d01894abe0674ef69f3
Pablo Villalba <pablo@saiku.es>
2010-01-16 14:40:05 +0100

    Gravatar will be displayed if avatar is not present

--- app/models/user/avatar.rb
+++ app/models/user/avatar.rb
@@ -1,12 +1,17 @@
 class User
 
+  AvatarSizes = {
+    :micro => [24,24],
+    :thumb => [48,48],
+    :profile => [278,500] }
+
   has_attached_file :avatar, 
     :url  => "/avatars/:id/:style.png",
     :path => ":rails_root/public/avatars/:id/:style.png",
     :styles => { 
-      :micro => "24x24#", 
-      :thumb => "48x48#", 
-      :profile => "278x500>" },
+      :micro => "#{AvatarSizes[:micro][0]}x#{AvatarSizes[:micro][1]}#", 
+      :thumb => "#{AvatarSizes[:thumb][0]}x#{AvatarSizes[:thumb][1]}#", 
+      :profile => "#{AvatarSizes[:profile][0]}x#{AvatarSizes[:profile][1]}>" },
     :convert_options => {
       :all => "-define png:bit-depth=24 -interlace PNG"
     }
@@ -24,10 +29,32 @@ class User
     @geometry[style] ||= Paperclip::Geometry.from_file(avatar.path(style))
   end
 
+  def avatar_or_gravatar_url(size)
+    if avatar_file_name
+      avatar.url(size)
+    else
+      gravatar(size)
+    end
+  end
+
   private
   
     def reprocess_avatar
       avatar.reprocess!
-    end    
+    end
+
+    def gravatar_email
+      email.downcase
+    end
+
+    def gravatar_id
+      Digest::MD5.hexdigest(gravatar_email)
+    end
+
+    def gravatar(size, default='wavatar')
+      url = "http://www.gravatar.com/avatar/#{gravatar_id}?s=#{AvatarSizes[size][0]}"
+      url += "&d=#{default}" if default
+      url
+    end
 
 end
