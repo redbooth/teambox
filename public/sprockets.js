be9e021809c11536253425bb23583b26893ed0b8
Balint Erdi <balint.erdi@gmail.com>
2010-01-26 21:36:29 +0100

    moving js code to sprockets.js

--- public/sprockets.js
+++ public/sprockets.js
@@ -9774,6 +9774,130 @@ Person = {
   }
 
 }
+Task = {
+
+  make_sortable: function(task_id, all_task_ids) {
+    Sortable.create(task_id, {
+      constraint:'vertical',
+      containment: all_task_ids,
+      format: /.*task_(\d+)_item/,
+      handle:'img.drag',
+      tag:'div',
+      onUpdate: function(){
+        new Ajax.Request($(task_id).readAttribute("reorder_url"), {
+          asynchronous: true,
+          evalScripts: true,
+          parameters: Sortable.serialize(task_id)
+        })
+      }
+    })
+  },
+
+  make_all_sortable: function() {
+    var task_div_ids = $$(".tasks").map(function(task_div){
+      return task_div.identify();
+    })
+    task_div_ids.each(function(task_div_id){
+      Task.make_sortable(task_div_id, task_div_ids);
+    })
+  },
+
+  create: function(form, url) {
+    new Ajax.Request(url, {
+      asynchronous: true,
+      evalScripts: true,
+      parameters: form.serialize(),
+      onLoading: function() {
+        form.down('.loading').show();
+      },
+      onSuccess: function(response){
+        form.down('.loading').hide();
+        form.hide();
+        form.up().down(".new_task_link").show();
+        var list_of_tasks = form.up().down(".tasks");
+        var task_item_html = response.responseText;
+        list_of_tasks.insert({ bottom: task_item_html })
+
+        Task.highlight_last_as_new(list_of_tasks);
+        Task.make_all_sortable();
+
+        var new_task = list_of_tasks.select('.task').last();
+        var show_in_main_content_url = new_task.readAttribute('show_in_main_content_url');
+        Task.show_in_main_content(show_in_main_content_url);
+      }
+    })
+  },
+
+  update: function(form, url) {
+    new Ajax.Request(url, {
+      asynchronous: true,
+      evalScripts: true,
+      parameters: form.serialize(),
+      onLoading: function() {
+        form.down('.loading').show();
+      },
+      onSuccess: function(response){
+        form.down('.loading').hide();
+        form.hide();
+        form.up().down(".new_task_link").show();
+      }
+    })
+  },
+
+  highlight_last_as_new: function(list_of_tasks) {
+    var new_task = list_of_tasks.select('.task').last();
+    list_of_tasks.select('.task').each(function(task){
+      task.removeClassName('active_new');
+    })
+    new_task.addClassName('active_new');
+  },
+
+  show_in_main_content: function(url) {
+    new Ajax.Request(url, {
+      asynchronous: true,
+      evalScripts: true,
+      method: 'get',
+      onSuccess: function(response){
+        Element.replace('content', response.responseText);
+        Task.bind_cancel_links_on_update_forms();
+        Task.bind_creation();
+        Task.bind_update();
+      }
+    })
+  },
+
+  bind_creation: function() {
+    $$(".inline_form_create").each(function(submit_button){
+      submit_button.observe('click', function(event){
+        var form = event.findElement("form");
+        var submit_url = form.readAttribute("action");
+        Task.create(form, submit_url);
+        event.stop();
+      })
+    })
+  },
+
+  bind_update: function() {
+    $$(".inline_form_update").each(function(submit_button){
+      submit_button.observe('click', function(event){
+        var form = event.findElement("form");
+        var submit_url = form.readAttribute("action");
+        Task.update(form, submit_url);
+        event.stop();
+      })
+    })
+  },
+
+  bind_cancel_links: function(cancel_link, classToShow) {
+    cancel_link.observe('click', function(event){
+      var form = event.findElement("form");
+      form.up().down(classToShow).show();
+      form.hide();
+    })
+  }
+
+}
+
 Event.addBehavior({
   ".task:mouseover": function(e){
     $(this).down('img.drag').show();
@@ -9782,6 +9906,16 @@ Event.addBehavior({
   ".task:mouseout": function(e){
     $$(".task img.drag").each(function(e){ e.hide(); });
     $$(".task span.task_status").each(function(e){ e.show(); });
+  },
+  ".inline_form_create_cancel:click": function(e){
+    var form = e.findElement("form");
+    form.up().down(".new_task_link").show();
+    form.hide();
+  },
+  ".inline_form_update_cancel:click": function(e){
+    var form = e.findElement("form");
+    form.up().down(".task_header").show();
+    form.hide();
   }
 });
 ToolTips = {
