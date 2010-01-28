767b73a01ddb28cb7ce3b2ff44ff380f88fc5665
Balint Erdi <balint.erdi@gmail.com>
2010-01-28 11:37:59 +0100

    adds unit test to contacts_not_in_my_project

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -300,4 +300,32 @@ describe User do
       Factory(:user).in_project(@project).should be_nil
     end
   end
+
+  describe "contacts not in project" do
+    before do
+      @me = Factory(:user)
+      @project = Factory(:project)
+      @project.add_user(@me)
+      @me.reload
+    end
+    it "does not return a user that is already in the project" do
+      user = Factory(:user)
+      @project.add_user(user)
+      @me.contacts_not_in_project(@project).should_not include(user)
+    end
+
+    it "does not return a user that does not belong to any of my projects" do
+      user = Factory(:user)
+      @me.contacts_not_in_project(@project).should_not include(user)
+    end
+
+    it "returns a user that is not in this project but in one of my other ones" do
+      user = Factory(:user)
+      other_project = Factory(:project)
+      other_project.add_user(@me)
+      other_project.add_user(user)
+      @me.reload; user.reload
+      @me.contacts_not_in_project(@project).should include(user)
+    end
+  end
 end
