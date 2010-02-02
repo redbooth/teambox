41c90e64b564dc76c3b7307956462d60180dfda0
Balint Erdi <balint.erdi@gmail.com>
2010-02-02 15:33:23 +0100

    fixes highlight in user mentions

--- lib/html_formatting.rb
+++ lib/html_formatting.rb
@@ -1,8 +1,8 @@
 module HtmlFormatting
   protected
-  
+
   include ActionView::Helpers::UrlHelper
-  
+
   def format_attributes
     self.class.formatted_attributes.each do |attr|
       raw = read_attribute attr
@@ -10,40 +10,40 @@ module HtmlFormatting
       text = format_textile(raw || '')
       text = format_usernames(text)
       text = format_links(text)
-      
+
       write_attribute "#{attr}_html", white_list_sanitizer.sanitize(text)
     end
   end
-  
+
   # Get @username, like in Twitter, and link it to user path
   def format_usernames(text)
-    text.gsub(/([\s>])@([a-z0-9_]+)([\W])/i) do |match|
-      user = User.find_by_login(match[2..-2])
+    text.gsub(/@([a-z0-9_]+)/i) do |match|
+      user = User.find_by_login(match[1..-1])
       if user && is_in_project?(user)
         if is_a? Comment
           @mentioned ||= []
           @mentioned |= [user]
         end
-        match[0,2] + link_to(user.login, "/users/#{user.login}") + match[-1,1]
+        match[0,1] + link_to(user.login, "/users/#{user.login}")
       else
         match
       end
     end
   end
-  
+
   def is_in_project?(user)
      Person.exists?(:user_id => user.id, :project_id => project.id)
   end
-  
+
   def format_textile(text)
     textilized = RedCloth.new(text, [:hard_breaks, :no_span_caps])
     textilized.hard_breaks = true if textilized.respond_to?("hard_breaks=")
     textilized.to_html
   end
-  
+
   def format_links(text)
     linked = auto_link(text) { |text| truncate(text, :length => 40) }
     linked.gsub(/href=\"www/i) { |s| "href=\"http://www" }
   end
-  
+
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -9,7 +9,7 @@ describe Comment do
       comment = Factory.build(:comment, :project => @project, :user => @user, :target => @project)
       comment.save.should be_true
       comment.user.should == @user
-      comment.target.should == @project    
+      comment.target.should == @project
       @project.comments.last.should == comment
       @user.comments.last.should == comment
     end
@@ -20,12 +20,12 @@ describe Comment do
       @project = Factory(:project)
       @user = @project.user
     end
-    
+
     it "should add it as an activity" do
       comment = Factory(:comment, :project => @project, :user => @user, :target => @project)
       @project.reload.activities.first.target.should == comment
     end
-    
+
     it "should notify mentioned @user in the project" do
       @mentioned = Factory(:user)
       @project.add_user(@mentioned)
@@ -47,7 +47,7 @@ describe Comment do
       Emailer.should_not_receive(:deliver_notify_comment)
       comment.save!
     end
-    
+
     it "should not notify mentions to @user if he doesn't belong to the project" do
       @mentioned = Factory(:user)
       @mentioned.notify_mentions = true
@@ -57,7 +57,7 @@ describe Comment do
       Emailer.should_not_receive(:deliver_notify_comment)
       comment.save!
     end
-    
+
     it "should not notify mentions to the users who posts them" do
       @mentioned = @user
       @mentioned.notify_mentions = true
@@ -72,7 +72,7 @@ describe Comment do
   #describe "posting to a conversation"
   #describe "posting to a task list"
   #describe "posting to a task"
-  
+
   #describe "marking as read"
 
   describe "formatting" do
@@ -80,31 +80,31 @@ describe Comment do
       @project = Factory(:project)
       @user = @project.user
     end
-    
+
     it "should format text" do
       body = "She *used* to _mean_ so much to ME!"
       comment = Factory.create(:comment, :body => body, :project => @project, :user => @user, :target => @project)
-      comment.body_html.should == "<p>She <strong>used</strong> to <em>mean</em> so much to ME!</p>"            
+      comment.body_html.should == "<p>She <strong>used</strong> to <em>mean</em> so much to ME!</p>"
     end
-    
+
     it "should format lists" do
       body = "She used to mean:\n* So\n* much\n* to\n * me!"
       comment = Factory.create(:comment, :body => body, :project => @project, :user => @user, :target => @project)
       comment.body_html.should == "<p>She used to mean:</p>\n<ul>\n\t<li>So</li>\n\t<li>much</li>\n\t<li>to</li>\n\t<li>me!</li>\n</ul>"
     end
-    
+
     it "should format emails and links" do
       body = 'she@couchsurfing.org used to mean so much to www.teambox.com'
       comment = Factory.create(:comment, :body => body, :project => @project, :user => @user, :target => @project)
       comment.body_html.should == "<p><a href=\"mailto:she@couchsurfing.org\">she@couchsurfing.org</a> used to mean so much to <a href=\"http://www.teambox.com\">www.teambox.com</a></p>"
     end
-    
+
     it "should turn to links textile links" do
       body = 'I loved that quote: "I like the Divers, but they want me want to go to a war":http://www.shmoop.com/tender-is-the-night/tommy-barban.html. Great page, too.'
       comment = Factory.create(:comment, :body => body, :project => @project, :user => @user, :target => @project)
       comment.body_html.should == "<p>I loved that quote: <a href=\"http://www.shmoop.com/tender-is-the-night/tommy-barban.html\">I like the Divers, but they want me want to go to a war</a>. Great page, too.</p>"
     end
-    
+
     it "should add http:// in front of links to www.site.com" do
       body = "I'd link my competitors' \"mistakes\":www.failblog.org, but that'd give them free traffic. So instead I link www.google.com."
       comment = Factory.create(:comment, :body => body, :project => @project, :user => @user, :target => @project)
@@ -116,16 +116,16 @@ describe Comment do
       comment = Factory.create(:comment, :body => body, :project => @project, :user => @user, :target => @project)
       comment.body_html.should == "<p>Did you know the logo from Teambox has <a href=\"http://en.wikipedia.org/wiki/Color_theory\">carefully selected colors</a>? <img src=\"http://app.teambox.com/images/header_logo_large.jpg\" /></p>"
     end
-    
+
     it "should preserve blocks of code and pre"
   end
 
-  describe "comments mentioning @user" do
+  describe "mentioning @user" do
     before do
       @project = Factory(:project)
       @user = Factory(:user, :login => 'existing')
     end
-    
+
     it "should link to users page when mentioning @existing if they are in the project" do
       @project.add_user(@user)
       body = "@existing, hey, @existing"
@@ -133,7 +133,19 @@ describe Comment do
       comment.body_html.should == "<p>@<a href=\"/users/existing\">existing</a>, hey, @<a href=\"/users/existing\">existing</a></p>"
       comment.mentioned.should == [@user]
     end
-    
+
+    it "should link to all the mentioned users if they are in the project" do
+      pablo = Factory(:user, :login => "pablo")
+      james = Factory(:user, :login => "james")
+      @project.add_user(pablo)
+      @project.add_user(james)
+      body = "@pablo @james Check this out!"
+      comment = Factory(:comment, :body => body, :project => @project, :user => @project.user, :target => @project)
+      comment.body_html.should == "<p>@<a href=\"/users/pablo\">pablo</a> @<a href=\"/users/james\">james</a> Check this out!</p>"
+      comment.mentioned.should include(pablo)
+      comment.mentioned.should include(james)
+    end
+
     it "should add the mentioned @user to watchers of a Conversation, Task List or Task" do
       @project.add_user(@user)
 
@@ -144,7 +156,7 @@ describe Comment do
         body = "I would like to add @existing to this thread, but not @unexisting."
         comment = Factory(:comment, :body => body, :project => @project, :user => @project.user, :target => element)
         comment.mentioned.should == [@user]
-      
+
         element.reload.watchers.collect(&:id).should == [@project.user, @user].collect(&:id)
       end
     end
@@ -155,7 +167,7 @@ describe Comment do
       comment.body_html.should == "<p>@existing is a cool guy, but he is not in this project</p>"
       comment.mentioned.should == nil
     end
-    
+
     it "should not link to users page when typing @unexisting" do
       body = "Hey, @unexisting, take a look at this!"
       comment = Factory.create(:comment, :body => body, :project => @project, :user => @project.user, :target => @project)
