1f6c841b11bb32ab9e207eeeb4338a07c59d7c5d
Pablo Villalba <pablo@teambox.com>
2010-02-05 23:06:50 +0100

    Forgot to include 'de' and 'fr' locales for public pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -84,7 +84,7 @@ class ApplicationController < ActionController::Base
       preferred_locale = nil
 
       if browser_locale = request.headers['HTTP_ACCEPT_LANGUAGE']
-        preferred_locale = ['en', 'es'].
+        preferred_locale = %w(en es fr de).
                             select { |i| browser_locale.include?(i) }.
                             collect { |i| [i, browser_locale.index(i)] }.
                             sort { |a,b| a[1] <=> b[1] }.
