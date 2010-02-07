10eb760e73b051592fc9aa3e918a701150b37e26
unixcharles <unixcharles@gmail.com>
2010-02-08 04:49:55 +0800

    added enable_starttls_auto for gmail smtp support, default to false

--- config/environment.rb
+++ config/environment.rb
@@ -20,12 +20,13 @@ Rails::Initializer.run do |config|
   if APP_CONFIG['allow_outgoing_email']
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
-     :address        => APP_CONFIG['outgoing']['host'],
-     :port           => APP_CONFIG['outgoing']['port'],
-     :domain         => APP_CONFIG['outgoing']['from'],
-     :user_name      => APP_CONFIG['outgoing']['user'],
-     :password       => APP_CONFIG['outgoing']['pass'],
-     :authentication => APP_CONFIG['outgoing']['auth'].to_sym
+     :enable_starttls_auto  => APP_CONFIG['outgoing']['enable_starttls_auto'],
+     :address               => APP_CONFIG['outgoing']['host'],
+     :port                  => APP_CONFIG['outgoing']['port'],
+     :domain                => APP_CONFIG['outgoing']['from'],
+     :user_name             => APP_CONFIG['outgoing']['user'],
+     :password              => APP_CONFIG['outgoing']['pass'],
+     :authentication        => APP_CONFIG['outgoing']['auth'].to_sym
     }
   end
   
