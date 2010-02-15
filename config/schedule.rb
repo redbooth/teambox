90409634b92bfcb47ee631c293ee088605f47a14
Balint Erdi <balint.erdi@gmail.com>
2010-02-15 09:49:56 +0100

    runs rebuilding of Thinking Sphynx cache as a cron job

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,11 @@
 set :output, "#{RAILS_ROOT}/log/cron.log"
 
+# send out daily reminders
 every 1.day, :at => '00:01 am' do
   runner "User.send_daily_task_reminders"
+end
+
+# rebuild thinking sphynx cache
+every 5.minutes do
+  rake "ts:rebuild"
 end
