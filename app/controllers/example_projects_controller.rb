401f6e4afc4c761fd4e1f49d99d6921a2cdd5adc
Pablo <pablo@teambox.com>
2010-02-19 12:57:56 +0100

    Bug in example projects

--- app/controllers/example_projects_controller.rb
+++ app/controllers/example_projects_controller.rb
@@ -4,13 +4,13 @@ class ExampleProjectsController < ApplicationController
   end
 
   def create
-    @project = current_user.find_or_create_example_project
-
     unless current_user.can_create_project?
       flash[:error] = "You can't create any projects with your current account."
       redirect_to root_path
       return
     end
+
+    @project = current_user.find_or_create_example_project
 
     if @project
       redirect_to @project
