c60d669aa092359775239fb3bff004d09a4faeae
Pablo <pablo@teambox.com>
2010-02-19 12:52:46 +0100

    Prevent example projects from being generated unless the user is allowed

--- app/controllers/example_projects_controller.rb
+++ app/controllers/example_projects_controller.rb
@@ -6,6 +6,12 @@ class ExampleProjectsController < ApplicationController
   def create
     @project = current_user.find_or_create_example_project
 
+    unless current_user.can_create_project?
+      flash[:error] = "You can't create any projects with your current account."
+      redirect_to root_path
+      return
+    end
+
     if @project
       redirect_to @project
     else
