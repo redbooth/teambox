3078c3e490c3ad8b7d515a58de6aa54fefd15672
Pablo <pablo@teambox.com>
2010-02-24 21:10:33 +0100

    Restyling according to Alex tips
    Comment padding
    Colors for task status and comment
    Changing JS hover to CSS hover

--- app/javascripts/comment.js
+++ app/javascripts/comment.js
@@ -1,16 +1,4 @@
 Event.addBehavior({
-  ".comment:mouseover": function(e){
-    $(this).className = 'comment comment_hover'
-  },
-  ".comment:mouseout": function(e){
-    $$("div.comment").each(function(e){ e.className = 'comment'; });
-  },
-  ".activity:mouseover": function(e){
-    $(this).className = 'activity activity_hover'
-  },
-  ".activity:mouseout": function(e){
-    $$("div.activity").each(function(e){ e.className = 'activity'; });
-  },
   "#sort_uploads:click": function(e){
     Comment.update();
   },
