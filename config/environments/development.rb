daef4374a946ea324031454bb686d76e5fac8e24
Balint Erdi <balint.erdi@gmail.com>
2010-03-05 21:33:11 +0100

    caching makes more sense in production :)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,8 +11,7 @@ config.whiny_nils = true
 # Show full error reports and disable caching
 config.action_controller.consider_all_requests_local = true
 config.action_view.debug_rjs                         = true
-config.action_controller.perform_caching             = true
-config.action_controller.cache_store                 = :file_store, Rails.root + "tmp/cache"
+config.action_controller.perform_caching             = false
 
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
