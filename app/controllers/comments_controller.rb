632f66dba9fa0d5544edb4e7783e40239158888a
jamesu <jamesu@gmail.com>
2010-03-09 20:52:11 +0000

    Even better solution!

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,29 +14,29 @@ class CommentsController < ApplicationController
 
     if @comment.save
       @comment.save_uploads(params)
-      @target = @comment.target
-      
-      case @target
-      when Conversation
-        @conversation = @comment.target
-        redirect_path = project_conversation_path(@current_project, @conversation)
-      when TaskList
-        @task_list = @comment.target
-        redirect_path = project_task_list_path(@current_project, @task_list)
-      when Task
-        @comment.reload
-        @task = @comment.target
-        @target = @task
-        @task_list = @task.task_list
-        @new_comment = @current_project.comments.new
-        @new_comment.target = @task
-        @new_comment.status = @task.status
-        redirect_path = project_task_list_task_path(@current_project, @task_list, @task)
-      else
-        redirect_path = project_path(@target)
-      end
+    end  
+    
+    @target = @comment.target
+    
+    # Evaluate target
+    case @target
+    when Conversation
+      @conversation = @comment.target
+      redirect_path = project_conversation_path(@current_project, @conversation)
+    when TaskList
+      @task_list = @comment.target
+      redirect_path = project_task_list_path(@current_project, @task_list)
+    when Task
+      @comment.reload
+      @task = @comment.target
+      @target = @task
+      @task_list = @task.task_list
+      @new_comment = @current_project.comments.new
+      @new_comment.target = @task
+      @new_comment.status = @task.status
+      redirect_path = project_task_list_task_path(@current_project, @task_list, @task)
     else
-      redirect_path = project_path(@current_project)
+      redirect_path = project_path(@target || @current_project)
     end
 
     respond_to do |f|
