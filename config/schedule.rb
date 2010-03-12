203e16d607bb6b0958f3c482452703757a4475e7
Pablo Villalba <pablo@teambox.com>
2010-03-12 13:30:54 +0100

    Schedule for inbox and daily reminders

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,10 +2,9 @@ set :output, "#{RAILS_ROOT}/log/cron.log"
 
 # send out daily reminders
 every 1.hour do
-  runner "User.send_daily_task_reminders"
+  runner "User.send_daily_task_reminders", :environment => :production
 end
 
-# rebuild thinking sphynx cache
-every 5.minutes do
-  rake "ts:rebuild"
+every 1.minutes do
+  rake "mail:inbox", :environment => :production
 end
