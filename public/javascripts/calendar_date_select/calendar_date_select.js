570088fb272e9314414c62013c4949928bfabddd
ujifgc <ujifgc@tb.ujif.name.ujif.name>
2010-03-13 11:57:57 +0400

    Fixed bug with date select in popup calendar (Opera 10.5 js)

--- public/javascripts/calendar_date_select/calendar_date_select.js
+++ public/javascripts/calendar_date_select/calendar_date_select.js
@@ -355,9 +355,9 @@ CalendarDateSelect.prototype = {
     if ((this.target_element.disabled || this.target_element.readOnly) && this.options.get("popup") != "force") return false;
     if (parts.get("day")) {
       var t_selected_date = this.selected_date, vdc = this.options.get("valid_date_check");
-      for (var x = 0; x<=3; x++) t_selected_date.setDate(parts.get("day"));
       t_selected_date.setYear(parts.get("year"));
       t_selected_date.setMonth(parts.get("month"));
+      t_selected_date.setDate(parts.get("day"));
       
       if (vdc && ! vdc(t_selected_date.stripTime())) { return false; }
       this.selected_date = t_selected_date;
