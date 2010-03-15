74278d24a6b9dc224fcd6825ddbcf25b9519b9f5
Pablo Villalba <pablo@teambox.com>
2010-03-15 22:42:54 +0100

    Upgrading to Rails 2.3.5

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,4 @@
-RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 APP_CONFIG = YAML.load_file("#{RAILS_ROOT}/config/teambox.yml")[RAILS_ENV]
