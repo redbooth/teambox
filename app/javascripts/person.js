c563399104b0961d9a65864005f0fa5f0b4923c7
Pablo Villalba <pablo@teambox.com>
2010-03-26 20:40:24 +0100

    Explaining user roles in the UI

--- app/javascripts/person.js
+++ app/javascripts/person.js
@@ -34,26 +34,3 @@ Event.addBehavior({
 	Effect.DropOut(el.up('.invite_user_link'));
   }
 });
-
-Person = {
-  watch_role: function(){
-    //$$('.roles .role').each(function(e){ 
-    //  e.observe('click', Person.change_role)
-    //});
-    $$('.roles .role input').each(function(e){ 
-      e.observe('click', Person.change_role_by_radio)
-    });    
-  },
-  change_role: function(e){
-    $$('.roles .active').each(function(ee){ ee.removeClassName('active') })
-    $$('.roles input').each(function(ee){ ee.checked = false })
-    var ee = e.element()  
-    ee.down('input').checked = true;
-    ee.addClassName('active');
-  },
-  change_role_by_radio: function(e){
-    $$('.roles .active').each(function(ee){ ee.removeClassName('active') })
-    var ee = e.element()  
-    ee.up('.role').addClassName('active');
-  }
-}
