09f06da32ea30e7ca4672881d016d386b45db065
Mislav Marohnić <mislav.marohnic@gmail.com>
2010-04-01 20:44:13 +0200

    useful CSS3 Sass mixins: gradient, box-shadow, transform

--- config/initializers/sass.rb
+++ config/initializers/sass.rb
@@ -1,1 +1,7 @@
-Sass::Plugin.options[:always_check] = true
+# for Haml versions 2.2.x
+module Sass::Script::Functions
+  def rgba(color, opacity)
+    values = color.value + [opacity]
+    Sass::Script::String.new('rgba(%d,%d,%d,%s)' % values)
+  end
+end
