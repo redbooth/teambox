f3a6446cf1f285eb77e098c1ef377277b2eb5724
jamesu <jamesu@gmail.com>
2010-05-11 17:14:30 +0100

    Fix .page == nil bug. FIXME: we need to improve activities!

--- app/models/page.rb
+++ app/models/page.rb
@@ -4,6 +4,7 @@ class Page < RoleRecord
   has_many :uploads
   
   has_many :slots, :class_name => 'PageSlot', :order => 'position ASC'
+  acts_as_paranoid
   
   attr_accessible :name, :description, :note_attributes
 
