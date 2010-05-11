284041871fcd3b9ec66d39afabcf38311268bf2b
Mislav Marohnić <mislav.marohnic@gmail.com>
2010-05-11 23:07:12 +0200

    prevent fyi tooltips from showing while the page is loading

--- vendor/sprockets/weakling/src/fyi.js
+++ vendor/sprockets/weakling/src/fyi.js
@@ -1,21 +1,26 @@
-document.on("dom:loaded", function() {
-  $$('.fyi').invoke('hide')
-})
+;(function() {
+  var selector = '.fyi',
+      head = document.getElementsByTagName('head')[0],
+      style = document.createElement('style')
 
-Event.addBehavior({
-  'input:focusin': function(e) {
-    var tooltip = this.next('.fyi')
-    if (tooltip) {
-      tooltip.show()
-      var offset = this.cumulativeOffset()
-      tooltip.setStyle({
-        left: offset.left + this.getLayout().get('border-box-width') + 10 + 'px',
-        top: offset.top + 'px'
-      })
+  style.appendChild(document.createTextNode(selector + ' { display:none }'))
+  head.appendChild(style)
+
+  Event.addBehavior({
+    'input:focusin': function(e) {
+      var tooltip = this.next(selector)
+      if (tooltip) {
+        tooltip.setStyle({ display: 'block' })
+        var offset = this.cumulativeOffset()
+        tooltip.setStyle({
+          left: offset.left + this.getLayout().get('border-box-width') + 10 + 'px',
+          top: offset.top + 'px'
+        })
+      }
+    },
+    'input:focusout': function(e){
+      var tooltip = this.next(selector)
+      if (tooltip) tooltip.hide()
     }
-  },
-  'input:focusout': function(e){
-    var tooltip = this.next('.fyi')
-    if (tooltip) tooltip.hide()
-  }
-})
+  })
+})()
