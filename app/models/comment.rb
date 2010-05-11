a236bd4239ada3062e9aae7b4abe3cf99bf14473
Jordi Romero <jordi@jrom.net>
2010-05-11 18:14:24 +0200

    Accept friendly hours input when commenting a Task. eg: 2h 30m or 2:30 or 2.5
    In the form for new comments, we now use the virtual attribute human_hours
    instead of the database field hours, this way we parse the input for that field
    and translate the commented formats to the stored float value.
    
    The accepted formats are:
    - 7 (hours)
    - 7.5 (hours with decimals)
    - 7h (hours)
    - 30m (minutes => fractions of hours)
    - 2h 30m (hours and minutes => hours with decimals)
    - 2:30 (hours and minutes => hours with decimals)

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -14,12 +14,43 @@ class Comment < ActiveRecord::Base
 
   named_scope :with_hours, :conditions => 'hours > 0'
 
-  attr_accessible :status, :previous_status, :assigned, :previous_assigned, :hours
+  attr_accessible :status, :previous_status, :assigned, :previous_assigned, :human_hours
   validate :check_body
 
   attr_accessor :mentioned # used by format_usernames to set who's being mentioned
   attr_accessor :activity
-  
+
+  def human_hours
+    self.hours
+  end
+
+  # Instead of using the float 'hours' field in a form, we use 'human_hours'
+  # and we can take:
+  # 7 (hours)
+  # 7.5 (hours with decimals)
+  # 7h (hours)
+  # 30m (minutes => fractions of hours)
+  # 2h 30m (hours and minutes => hours with decimals)
+  # 2:30 (hours and minutes => hours with decimals)
+  def human_hours=(duration)
+    self.hours = if duration =~ /(\d+)h[ ]*(\d+)m/i
+      # 2h 15m
+      $1.to_f + $2.to_f / 60
+    elsif duration =~ /(\d+):(\d+)/
+      # 2:15
+      $1.to_f + $2.to_f / 60.0
+    elsif duration =~ /(\d+)m/i
+      # 20m
+      $1.to_f / 60.0
+    elsif duration =~ /(\d+)h/i
+      # 3h
+      $1.to_f
+    else
+      # old-style numeric format
+      duration.to_f
+    end
+  end
+
   def check_body
     if body and body.strip.empty?
       if !target.is_a? Task
