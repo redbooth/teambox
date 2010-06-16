936601c5fb69acc76ba182cc9932b5d01b8a9651
James Urquhart <jamesu@gmail.com>
2010-06-16 13:07:09 +0100

    Add spec for assigning tasks to invalid users for emails

--- spec/models/emailer/incoming_spec.rb
+++ spec/models/emailer/incoming_spec.rb
@@ -6,6 +6,7 @@ describe Emailer do
     before do
       @owner = Factory.create(:user)
       @fred = Factory.create(:user)
+      @janet = Factory.create(:user)
       @project = Factory.create(:project, :user_id => @owner.id)
       @project.add_user(@fred)
       @task = Factory(:task, :user_id => @owner.id, :project_id => @project.id)
@@ -43,6 +44,16 @@ describe Emailer do
       comment.status.should == Task::STATUSES[:open]
       comment.previous_assigned_id.should == nil
       comment.previous_status.should == Task::STATUSES[:new]
+    end
+    
+    it "should not assign the task to janet with #janet" do
+      @email_template.to = "#{@project.permalink}+task+#{@task.id}@#{Teambox.config.smtp_settings[:domain]}"
+      @email_template.body = "##{@janet.login}\nWe did some stuff"
+      Emailer.receive(@email_template.to_s)
+      
+      @task.reload
+      comment = @task.comments.last
+      @task.assigned_id.should_not == @janet.id
     end
     
     it "should resolve the task with #resolve or #resolved" do
