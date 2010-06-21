b8213d3e658461d7430f37d2bbe8530ee5608bad
Pablo Villalba <pablo@teambox.com>
2010-06-21 15:17:55 +0200

    Wrapping nil OAuth providers

--- app/models/app_link.rb
+++ app/models/app_link.rb
@@ -5,7 +5,7 @@ class AppLink < ActiveRecord::Base
   validates_uniqueness_of :user_id, :scope => :provider
 
   def self.providers
-    Array(APP_CONFIG['oauth_providers']).collect { |key,provider| provider['name'] }
+    APP_CONFIG['oauth_providers'].wrap.collect { |key,provider| provider['name'] }
   end
 
 end
