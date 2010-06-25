e3461bf42c290a3c545d502ac762ba44c9b89b94
Pablo Villalba <pablo@teambox.com>
2010-06-25 19:50:19 +0200

    Not requiring a permalink when creating a project

--- app/models/project/validation.rb
+++ app/models/project/validation.rb
@@ -1,6 +1,6 @@
 class Project
 
-  validates_length_of :name, :minimum => 3
+  validates_length_of :name, :minimum => 5
   validates_uniqueness_of :permalink, :case_sensitive => false
   validates_length_of :permalink, :minimum => 5
   validates_format_of :permalink, :with => /^[a-z0-9_\-]{5,}$/, :if => :permalink_length_valid?
