cb1a008f4e1fdb7acc1aaa3730391c8911280fd4
Pablo Villalba <pablo@teambox.com>
2010-06-27 20:51:18 +0800

    String#extract_emails: Adding a spec for domains like *.co.uk

--- spec/helpers/extract_emails_spec.rb
+++ spec/helpers/extract_emails_spec.rb
@@ -8,6 +8,7 @@ describe String do
     "  c@c.com word d@d.com\ne@f.com".extract_emails.should == %w(c@c.com d@d.com e@f.com)
     "inv@lid brr".extract_emails.should == []
     "'Pablo' <pablo2@teambox.com>, 'Juan Jaramillo'".extract_emails.should == %w(pablo2@teambox.com)
+    "james@cuppadev.co.uk".extract_emails.should == %w(james@cuppadev.co.uk)
   end
   
 end
