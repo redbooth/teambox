114950c834f453709741d99c8971f765212dbb67
Pablo Villalba <pablo@teambox.com>
2010-06-27 20:43:11 +0800

    Add String#extract_emails method
    Returns an Array with all the valid emails found in String
     "pablo@teambox.com"                #=> ["pablo@teambox.com"]
     "invalid@email"                    #=> []
     "  c@c.com word d@d.com\ne@f.com"  #=> ["c@c.com", "d@d.com", "e@f.com"]

---
+++ spec/helpers/extract_emails_spec.rb
@@ -0,0 +1,13 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe String do
+
+  it "should extract valid emails" do
+    "pablo@teambox.com".extract_emails.should == ["pablo@teambox.com"]
+    "a@a.com b@b.com".extract_emails.should == %w(a@a.com b@b.com)
+    "  c@c.com word d@d.com\ne@f.com".extract_emails.should == %w(c@c.com d@d.com e@f.com)
+    "inv@lid brr".extract_emails.should == []
+    "'Pablo' <pablo2@teambox.com>, 'Juan Jaramillo'".extract_emails.should == %w(pablo2@teambox.com)
+  end
+  
+end
