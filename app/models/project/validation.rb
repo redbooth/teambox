80dd8f858e964b6f3265e7470fd3466a08af0bec
Denis Novikov <denis.novikov@freakingloud.com>
2010-06-27 21:44:46 +0200

    Merge with e3461bf42c290a3c545d from micho/teambox

--- app/models/project/validation.rb
+++ app/models/project/validation.rb
@@ -1,6 +1,6 @@
 class Project
 
-  validates_length_of :name, :minimum => 3
+  validates_length_of :name, :minimum => 5
   validates_uniqueness_of :permalink, :case_sensitive => false
   validates_length_of :permalink, :minimum => APP_CONFIG['project_permalink_min_length']
   validates_format_of :permalink, :with => /^[a-z0-9_\-]{#{APP_CONFIG['project_permalink_min_length']},}$/, :if => :permalink_length_valid?
