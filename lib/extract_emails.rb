redbooth/teambox

commit b26ccc94df551d6cfd7567faf1d8a0bdd08566c2

add specs to point out everything that's wrong with the current email regex and fix it

lib/extract_emails.rb
@@ -1,8 +1,9 @@
 class String
-  Email_name_regex  = '[\w\.%\-\+]+'.freeze
-  Domain_head_regex = '(?:[A-Z0-9\-]+\.)+'.freeze
-  Domain_tld_regex  = '(?:[A-Z]{2,3}|com|org|net|edu|gov|mil|biz|info|mobi|name|aero|jobs|coop|museum)'.freeze
-  Email_regex       = /\A#{Email_name_regex}@#{Domain_head_regex}#{Domain_tld_regex}\z/i
+  # adapted from http://fightingforalostcause.net/misc/2006/compare-email-regex.php
+  EmailLocal  = /[\w!#\$%&'*\/=?^`{|}~+-]+/
+  EmailDomain = /(?:(?:(?:[a-z0-9][a-z0-9-]{0,62}[a-z0-9])|[a-z])\.)+[a-z]{2,6}/i
+  EmailHost   = /(?:\d{1,3}\.){3}\d{1,3}(?:\:\d{1,5})?/
+  EmailRegex  = /(?:#{EmailLocal}\.)*#{EmailLocal}@(?:#{EmailDomain}|#{EmailHost})/
 
   # Returns an Array with all the valid emails found in String
   # Examples:
@@ -10,6 +11,6 @@ class String
   #   "invalid@email"                    #=> []
   #   "  c@c.com word d@d.com\ne@f.com"  #=> ["c@c.com", "d@d.com", "e@f.com"]
   def extract_emails
-    split(/[^\w\.%\-@\+]/).select { |email| email.match Email_regex }
+    scan(EmailRegex)
   end
 end

spec/helpers/extract_emails_spec.rb
@@ -1,15 +1,93 @@
-require File.dirname(__FILE__) + '/../spec_helper'
+require 'spec_helper'
 
-describe String do
+describe String, '#extract_emails' do
 
-  it "should extract valid emails" do
-    "pablo@teambox.com".extract_emails.should == ["pablo@teambox.com"]
-    "a@a.com b@b.com".extract_emails.should == %w(a@a.com b@b.com)
-    "  c@c.com word d@d.com\ne@f.com".extract_emails.should == %w(c@c.com d@d.com e@f.com)
-    "inv@lid brr".extract_emails.should == []
-    "'Pablo' <pablo2@teambox.com>, 'Juan Jaramillo'".extract_emails.should == %w(pablo2@teambox.com)
-    "james@cuppadev.co.uk".extract_emails.should == %w(james@cuppadev.co.uk)
-    "ohyeah+teambox@gmail.com".extract_emails.should == %w(ohyeah+teambox@gmail.com)
+  context "valid" do
+    it "extracts single email from: pablo@teambox.com" do
+      should == %w[ pablo@teambox.com ]
+    end
+
+    it "extracts multiple emails from: c@c.com word d@d.com\ne@f.com" do
+      should == %w[ c@c.com d@d.com e@f.com ]
+    end
+
+    it "extracts header-style emails from: 'Pablo' <pablo2@teambox.com>, 'Juan Jaramillo'" do
+      should == %w[ pablo2@teambox.com ]
+    end
+
+    it "handles two-part TLD from: james@cuppadev.co.uk" do
+      should == %w[ james@cuppadev.co.uk ]
+    end
+
+    it "handles plus-addressing from: ohyeah+teambox@gmail.com" do
+      should == %w[ ohyeah+teambox@gmail.com ]
+    end
+
+    it "extracts email with numbers from: l3tt3rsAndNumb3rs@domain.com" do
+      should == %w[ l3tt3rsAndNumb3rs@domain.com ]
+    end
+
+    it "extracts email with dash from: has-dash@domain.com" do
+      should == %w[ has-dash@domain.com ]
+    end
+
+    it "doesn't trip on an apostrophe from: hasApostrophe.o'leary@domain.org" do
+      should == %w[ hasApostrophe.o'leary@domain.org ]
+    end
+
+    it "extracts the email with a 'travel' TLD from: uncommon@domain.travel" do
+      should == %w[ uncommon@domain.travel ]
+    end
+
+    it "extracts the email with numbers in domain from: numbers@911.com" do
+      should == %w[ numbers@911.com ]
+    end
+
+    it "extracts the email with uppercase domain from: hello@gMAIL.COM" do
+      should == %w[ hello@gMAIL.COM ]
+    end
+
+    it "handles domain specified with an IP & port from: IPAndPort@127.0.0.1:25" do
+      should == %w[ IPAndPort@127.0.0.1:25 ]
+    end
+
+    it "extracts email with subdomains from: subdomain@sub.domain.com" do
+      should == %w[ subdomain@sub.domain.com ]
+    end
+
+    it "doesn't trip on whacky characters from: &*=?^+{}'~@validCharsInLocal.net" do
+      should == %w[ &*=?^+{}'~@validCharsInLocal.net ]
+    end
+  end
+  
+  context "invalid" do
+    it "doesn't extract invalid email from: inv@lid brr" do
+      should be_empty
+    end
+
+    it "detects missing domain from: missingDomain@.com" do
+      should be_empty
+    end
+
+    it "trips on missing dot from: missingDot@com" do
+      should be_empty
+    end
+
+    it "dislikes two @ signs from: two@@signs.com" do
+      should be_empty
+    end
+
+    it "can't handle missing TLD from: missingTLD@domain." do
+      should be_empty
+    end
+  end
+  
+  protected
+  
+  def subject
+    desc = [description].concat(description_args).join(' ')
+    data = desc.match(/from: (.+)$/m)[1]
+    data.extract_emails
   end
   
 end

app/models/project/invitations.rb
@@ -1,3 +1,5 @@
+require_dependency 'extract_emails'
+
 class Project
 
   attr_reader :invited_users, :invited_emails

config/initializers/active_record.rb
@@ -1,5 +1,4 @@
 require_dependency 'html_formatting'
-require_dependency 'extract_emails'
 
 ActiveRecord::Base.class_eval do
   @@white_list_sanitizer = HTML::WhiteListSanitizer.new

commit 0fd100cf5799ea2a5e714ec529eb1e0ab2b6a7d8

specify that emails should start with a non-punctuation character
fixes invitations spec broken in b26ccc9

lib/extract_emails.rb
@@ -1,9 +1,9 @@
 class String
   # adapted from http://fightingforalostcause.net/misc/2006/compare-email-regex.php
-  EmailLocal  = /[\w!#\$%&'*\/=?^`{|}~+-]+/
+  EmailLocal  = /[\w!#\$%&'*\/=?^`{|}~+-]/
   EmailDomain = /(?:(?:(?:[a-z0-9][a-z0-9-]{0,62}[a-z0-9])|[a-z])\.)+[a-z]{2,6}/i
   EmailHost   = /(?:\d{1,3}\.){3}\d{1,3}(?:\:\d{1,5})?/
-  EmailRegex  = /(?:#{EmailLocal}\.)*#{EmailLocal}@(?:#{EmailDomain}|#{EmailHost})/
+  EmailRegex  = /[a-z0-9~](?:#{EmailLocal}+\.)*#{EmailLocal}*@(?:#{EmailDomain}|#{EmailHost})/i
 
   # Returns an Array with all the valid emails found in String
   # Examples:

spec/helpers/extract_emails_spec.rb
@@ -35,6 +35,10 @@ describe String, '#extract_emails' do
       should == %w[ hasApostrophe.o'leary@domain.org ]
     end
 
+    it "doesn't extract leading apostrophe from: 'hello@domain.org'" do
+      should == %w[ hello@domain.org ]
+    end
+
     it "extracts the email with a 'travel' TLD from: uncommon@domain.travel" do
       should == %w[ uncommon@domain.travel ]
     end
@@ -55,8 +59,8 @@ describe String, '#extract_emails' do
       should == %w[ subdomain@sub.domain.com ]
     end
 
-    it "doesn't trip on whacky characters from: &*=?^+{}'~@validCharsInLocal.net" do
-      should == %w[ &*=?^+{}'~@validCharsInLocal.net ]
+    it "doesn't trip on whacky characters from: ~&*=?^+{}'@validCharsInLocal.net" do
+      should == %w[ ~&*=?^+{}'@validCharsInLocal.net ]
     end
   end
   