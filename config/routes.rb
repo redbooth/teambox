fcc9259311b44d8d9b007a008d4c6df643f9e227
James Urquhart <jamesu@gmail.com>
2010-07-16 09:14:46 +0100

    Add some documentation for the multiple invites

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ ActionController::Routing::Routes.draw do |map|
 
   map.welcome           '/welcome',             :controller => 'users',       :action => 'welcome'
   map.text_styles       '/text_styles',         :controller => 'users',       :action => 'text_styles'
+  map.invite_format     '/invite_format',       :controller => 'invitations', :action => 'invite_format'
   map.feeds             '/feeds',               :controller => 'users',       :action => 'feeds'
   map.calendars         '/calendars',           :controller => 'users',       :action => 'calendars'
   map.close_wecome_tab  '/close_welcome_tab',   :controller => 'users',       :action => 'close_welcome'
