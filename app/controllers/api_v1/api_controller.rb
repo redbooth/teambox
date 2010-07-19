7933d6d2f3bf018a3940ff3853d5b1a574f4d19c
James Urquhart <jamesu@gmail.com>
2010-07-20 03:11:00 +0800

    Fleshing out projects controller concept

--- app/controllers/api_v1/api_controller.rb
+++ app/controllers/api_v1/api_controller.rb
@@ -1,6 +1,24 @@
 class ApiV1::APIController < ApplicationController
   # Common api helpers
   
+  def api_created(path)
+    respond_to do |format|
+      f.json { redirect_to path }
+    end
+  end
+  
+  def api_updated(object, status)
+    respond_to do |format|
+      f.json { render :as_json => object.to_xml, :status => status }
+    end
+  end
+  
+  def api_deleted
+    respond_to do |format|
+      f.json { render :text => '', :status => :deleted }
+    end
+  end
+  
   def api_error(message, status)
     error = {'error' => message}
     respond_to do |format|
--- app/controllers/api_v1/projects_controller.rb
+++ app/controllers/api_v1/projects_controller.rb
@@ -1,5 +1,5 @@
 class ApiV1::ProjectsController < ApiV1::APIController
-  before_filter :load_project, :except => [:index, :new]
+  before_filter :can_modify?, :only => [:edit, :update, :transfer, :destroy]
   
   def index
     @projects = current_user.projects
@@ -13,10 +13,15 @@ class ApiV1::ProjectsController < ApiV1::APIController
   end
   
   def new
+    @project = Project.new
+  end
+  
+  def create
     @project = current_user.projects.new(params[:project])
     
     unless current_user.can_create_project?
-      return api_error(t('projects.new.not_allowed'), :unprocessable_entity)
+      api_error(t('projects.new.not_allowed'), :unprocessable_entity)
+      return
     end
     
     respond_to do |f|
@@ -28,18 +33,63 @@ class ApiV1::ProjectsController < ApiV1::APIController
     end
   end
   
-  def create
-  end
-  
   def edit
   end
   
   def update
+    respond_to do |f|
+      if @current_project.update_attributes(params[:project])
+        handle_api_success(f, @current_project)
+      else
+        handle_api_error(f, @current_project)
+      end
+    end
+  end
+  
+  def transfer
+    unless @current_project.owner?(current_user)
+      api_error(t('common.not_allowed'), :unprocessable_entity)
+      return
+    end
+    
+    # Grab new owner
+    user_id = params[:project][:user_id] rescue nil
+    person = @current_project.people.find_by_user_id(user_id)
+    saved = false
+    
+    # Transfer!
+    unless person.nil?
+      @current_project.user = person.user
+      person.update_attribute(:role, Person::ROLES[:admin]) # owners need to be admin!
+      saved = @current_project.save
+    end
+    
+    if saved
+      api_updated(@project, :edited)
+    else
+      api_error(@project.errors, :unprocessable_entity)
+    end
   end
 
   def destroy
+    @current_project.destroy
+    respond_to do |f|
+      handle_api_success(f,@current_project)
+    end
   end
 
   protected
+  
+  def can_modify?
+    if !( @current_project.owner?(current_user) or 
+          ( @current_project.admin?(current_user) and 
+            !(params[:controller] == 'transfer' or params[:sub_action] == 'ownership')))
+      
+      api_error(t('common.not_allowed'), :unprocessable_entity)
+      return false
+    end
+    
+    true
+  end
   
 end
