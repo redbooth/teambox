0b46cae1d61d528028fefbcb959db1e0d978cb3f
Paco Guzman <fjguzman@aspgems.com>
2010-07-22 14:39:41 +0800

    Rails.env and StringInquirer instead RAILS_ENV inside app folder

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -62,7 +62,7 @@ class UsersController < ApplicationController
     load_app_link
 
     @user.confirmed_user = ((@invitation && @invitation.email == @user.email) or
-                            RAILS_ENV == "development" or
+                            Rails.env.develpment? or
                             !!@app_link)
 
     unless @invitation || signups_enabled?
