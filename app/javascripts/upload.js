e098d7d93ff21e019254286e1c7458162bb6acc4
Mislav Marohnić <mislav.marohnic@gmail.com>
2010-07-23 01:20:14 +0800

    enable multiple file uploads in upload area below comments

--- app/javascripts/upload.js
+++ app/javascripts/upload.js
@@ -5,3 +5,12 @@ document.on('click', '.index_uploads #column .add_button', function(e, button) {
     button.hide()
   }
 })
+
+document.on('change', '.upload_area input[type=file]', function(e, input) {
+  var newInput = new Element('input', {
+    type: 'file',
+    name: input.name.sub(/\d+/, function(m) { return parseInt(m[0]) + 1 })
+  })
+  
+  input.insert({ after: newInput })
+})
