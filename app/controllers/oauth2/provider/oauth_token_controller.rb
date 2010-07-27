22c4c68e07cb0ca278e50ef003fd7dd985d98e51
unixcharles <unixcharles@gmail.com>
2010-07-27 19:08:56 -0400

    add support for 'password' access grant method

--- app/controllers/oauth2/provider/oauth_token_controller.rb
+++ app/controllers/oauth2/provider/oauth_token_controller.rb
@@ -5,44 +5,59 @@ module Oauth2
       no_login_required
       
       def get_token
-        
-        authorization = OauthAuthorization.find_by_code(params[:code])
-        authorization.delete unless authorization.nil?
-        
-        original_token = OauthToken.find_by_refresh_token(params[:refresh_token])
-        original_token.delete unless original_token.nil?
-    
-        unless ['authorization-code', 'refresh-token'].include?(params[:grant_type])
+        unless ['authorization-code', 'refresh-token', 'password'].include?(params[:grant_type])
           render_error('unsupported-grant-type')
           return
         end
     
         client = OauthClient.find_by_client_id_and_client_secret(params[:client_id], params[:client_secret])
-    
+
+        case params[:grant_type]
+          when 'password' # see 'Resource Owner Password Credentials' grant_type, http://tools.ietf.org/html/draft-ietf-oauth-v2-10#section-4.1.2
+            user = User.authenticate(params[:username], params[:password])
+            authorization = client.create_authorization_for_user_id(user.id) if user
+          when 'authorization-code'
+            authorization = OauthAuthorization.find_by_code(params[:code])
+        end
+        
+        original_token = OauthToken.find_by_refresh_token(params[:refresh_token])
+        original_token.delete unless original_token.nil?
+        
         if client.nil?
           render_error('invalid-client-credentials')
           return
         end
     
-        if client.redirect_uri != params[:redirect_uri]
+        if client.redirect_uri != params[:redirect_uri] and params[:grant_type] != 'password'
           render_error('invalid-grant')
           return
         end
     
-        if params[:grant_type] == 'authorization-code'
+        case params[:grant_type]
+        when 'authorization-code'
           if authorization.nil? || authorization.expired? || authorization.oauth_client != client
             render_error('invalid-grant')
             return
           end
           token = authorization.generate_access_token
-        else # refresh-token
-          if original_token.nil? || original_token.oauth_client != client
+        when 'password'
+          if authorization.nil? || authorization.expired? 
             render_error('invalid-grant')
             return
           end
+          token = authorization.generate_access_token
+        when 'refresh-token'
+          if original_token.nil? || original_token.oauth_client != client
+            render_error('invalid-grant' + client.id)
+            return
+          end
           token = original_token.refresh
+        else
+          render_error('invalid-grant')
+          return
         end
-
+        authorization.delete unless authorization.nil?
+        
         render :content_type => 'application/json', :text => token.access_token_attributes.to_json
       end
   
