redbooth/teambox

commit a8b05596d96d233c306ff761beacdc33a6e71891

perform search over GET; unobtrusive term highlighting; improve styles

features/search_projects.feature
@@ -21,7 +21,7 @@ Feature: Search comments in projects
     
     When the search index is rebuilt
     And I fill in "search" with "the mine"
-    And I press "search"
+    And I press "Search"
     Then I should see "1 results found"
     And I should see "I found a hunk of gold today in the mine!"
     But I should not see "finish this space elevator"

app/controllers/search_controller.rb
@@ -1,12 +1,12 @@
 class SearchController < ApplicationController
   
-  before_filter :permission_to_search, :only => :results
+  before_filter :permission_to_search, :only => :index
   
-  def results
-    @search = params[:search]
+  def index
+    @search_terms = params[:q]
     
-    unless @search.blank?
-      @comments = Comment.search @search,
+    unless @search_terms.blank?
+      @comments = Comment.search @search_terms,
         :retry_stale => true, :order => 'created_at DESC',
         :with => { :project_id => project_ids },
         :page => params[:page]

app/javascripts/application.js
@@ -36,7 +36,6 @@ Event.onReady = function(fn) {
 
 Event.addBehavior = function(hash) {
   var behaviors = $H(hash)
-  // console.log(behaviors.keys())
   behaviors.each(function(pair) {
     var selector = pair.key.split(':'), fn = pair.value
     document.on(selector[1], selector[0], function(e, el) { fn.call(el, e) })
@@ -142,49 +141,6 @@ Element.addMethods({
     var currentText = element.innerHTML;
     var nextIndex = (texts.indexOf(currentText) + 1) % texts.length;
     return texts[nextIndex];
-  },
-  
-  addHighlights: function(element, terms, className) {
-    for (var i=0; i<terms.length; i++)
-      this.addHighlight(element, terms[i], className);
-  },
-  
-  // Courtesy of http://stackoverflow.com/questions/1650389/prototype-js-highlight-words-dom-traversing-correctly-and-efficiently
-  addHighlight: function(element, term, className) {
-    function innerHighlight(element, term, className) {
-      className = className || 'highlight';
-      term = (term || '').toUpperCase();
-
-      var skip = 0;
-      if ($(element).nodeType == 3) {
-        var pos = element.data.toUpperCase().indexOf(term);
-        if (pos >= 0) {
-          var middlebit = element.splitText(pos),
-              endbit = middlebit.splitText(term.length),
-              middleclone = middlebit.cloneNode(true),
-              spannode = document.createElement('span');
-
-          spannode.className = 'highlight';
-          spannode.appendChild(middleclone);
-          middlebit.parentNode.replaceChild(spannode, middlebit);
-          skip = 1;
-        }
-      }
-      else if (element.nodeType == 1 && element.childNodes && !/(script|style)/i.test(element.tagName)) {
-        for (var i = 0; i < element.childNodes.length; ++i)
-          i += innerHighlight(element.childNodes[i], term, className);
-      }
-      return skip;
-    }
-    innerHighlight(element, term, className);
-    return element;
-  },
-  removeHighlight: function(element, term, className) {
-    className = className || 'highlight';
-    $(element).select("span."+className).each(function(e) {
-      e.parentNode.replaceChild(e.firstChild, e);
-    });
-    return element;
   }
 });
 

app/javascripts/search.js
@@ -1,0 +1,54 @@
+document.on('dom:loaded', function() {
+  if ($$('body.index_search').any()) {
+    var param = $$('input[type=search]').first().readAttribute('name'),
+        terms = window.location.search.toQueryParams()[param]
+    
+    $('results').addHighlights($w(terms), 'highlight')
+  }
+})
+
+Element.addMethods({
+  addHighlights: function(element, terms, className) {
+    element = $(element)
+    terms.each(function(term) {
+      element.addHighlight(term, className)
+    })
+  },
+  // http://stackoverflow.com/questions/1650389/prototype-js-highlight-words-dom-traversing-correctly-and-efficiently
+  addHighlight: function(element, term, className) {
+    function innerHighlight(element, term, className) {
+      className = className || 'highlight';
+      term = (term || '').toUpperCase();
+
+      var skip = 0;
+      if ($(element).nodeType == 3) {
+        var pos = element.data.toUpperCase().indexOf(term);
+        if (pos >= 0) {
+          var middlebit = element.splitText(pos),
+              endbit = middlebit.splitText(term.length),
+              middleclone = middlebit.cloneNode(true),
+              spannode = document.createElement('span');
+
+          spannode.className = 'highlight';
+          spannode.appendChild(middleclone);
+          middlebit.parentNode.replaceChild(spannode, middlebit);
+          skip = 1;
+        }
+      }
+      else if (element.nodeType == 1 && element.childNodes && !/(script|style)/i.test(element.tagName)) {
+        for (var i = 0; i < element.childNodes.length; ++i)
+          i += innerHighlight(element.childNodes[i], term, className);
+      }
+      return skip;
+    }
+    innerHighlight(element, term, className);
+    return element;
+  },
+  removeHighlight: function(element, term, className) {
+    className = className || 'highlight';
+    $(element).select("span."+className).each(function(e) {
+      e.parentNode.replaceChild(e.firstChild, e);
+    });
+    return element;
+  }
+});

app/styles/_search.sass
@@ -13,20 +13,25 @@
         width: 10em
         +box-shadow(inset rgba(0,0,0,.2) 0px 1px 2px)
 
-.results_search
-  .comment
-    .block
-      width: 860px
-  .results
-    padding-left: 80px
-    font-size: 12px
-    margin: 20px
-    form
-      input#search
-        font-size: 20px
-        width: 450px
-        margin-right: 20px
-
-#results_set
-  span.highlight
-    background-color: yellow
+.index_search
+  #report
+    +light-box
+    margin: 10px 0
+    padding: 10px 1.2em
+    h1
+      margin-top: 0
+    form input[type=search]
+      width: 20em
+  
+  h2
+    font-size: 1.1em
+    padding-bottom: 0
+    border-bottom: none
+    margin: 1em 0 .2em 10px
+    
+  #results
+    .comment
+      .block
+        width: 880px
+    .highlight
+      background-color: yellow

app/styles/_shared.sass
@@ -70,6 +70,11 @@ $typeface: "Lucida Grande", Helvetica, "Helvetica Neue", Arial, Verdana, sans-se
   @if $y_size > 0
     height: $y_size
 
+=light-box
+  border: 1px rgb(220, 220, 220) solid
+  background-color: rgb(240, 240, 240)
+  +rounded(5px)
+
 .fyi
   position: absolute
   background: white

app/styles/_users.sass
@@ -174,11 +174,9 @@ $micro_size: 24px
     padding: 5px
     form.new_invitation
       font-size: 12px
-      border: 1px rgb(220, 220, 220) solid
-      background-color: rgb(240, 240, 240)
+      +light-box
       padding: 10px
       margin-bottom: 10px
-      +rounded(5px)
       h2
         font-size: 16px
         margin: 0 0 10px 0

app/views/search/index.html.haml
@@ -1,0 +1,17 @@
+#report
+  %h1
+    - if @current_project
+      = t('.title_in_project', :project => @current_project.name)
+    - else
+      = t('.title')
+
+  - form_tag search_path, :method => 'get' do
+    = text_field_tag :q, @search_terms, :type => 'search', :id => nil
+    = submit_tag(@comments ? t('.search_again') : t('search.submit'), :name => nil)
+
+- if @comments
+  %h2= t('.report', :count => @comments.total_entries, :term => @search_terms)
+  
+  #results= render @comments
+
+  = will_paginate @comments

commit f0208602ac6970e45e6402016a6602b187dfd17d

fix the search cucumber feature broken in a8b0559

features/search_projects.feature
@@ -20,7 +20,7 @@ Feature: Search comments in projects
     And I go to the projects page
     
     When the search index is rebuilt
-    And I fill in "search" with "the mine"
+    And I fill in the search box with "the mine"
     And I press "Search"
     Then I should see "1 results found"
     And I should see "I found a hunk of gold today in the mine!"

features/step_definitions/search_steps.rb
@@ -3,3 +3,7 @@ When /^the search index is re(indexed|built)$/ do |action|
   # seems to be necessary before hitting sphinx
   sleep(0.2)
 end
+
+When /^I fill in the search box with "(.+)"$/ do |value|
+  When(%(I fill in "q" with "#{value}"))
+end