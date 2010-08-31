35f6677fc3e949552ccbe19bb8462c63167747e4
Jordi Romero <jordi@jrom.net>
2010-08-31 11:59:31 +0200

    Make schema coherent with current migrations
    Bump schema to the last migration introduced in eda44ec1d23454efcb06ea39e7072ce7bb38ead7

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100817111219) do
+ActiveRecord::Schema.define(:version => 20100826150615) do
 
   create_table "activities", :force => true do |t|
     t.integer  "user_id"
