redbooth/teambox

commit fc757afc424ce8e606e8471b78e3d8f38727ac82

handle empty comments on tasks
Prevent the last task comment from being rendered
if a comment was not created in the first place.
Also prevent exceptions on tasks without comments.

app/models/task.rb
@@ -35,6 +35,7 @@ class Task < RoleRecord
   before_save :set_comments_author, :if => :updating_user
   before_save :save_changes_to_comment, :if => :track_changes?
   before_save :transition_from_new_to_open, :if => :assigned_id?
+  before_update :remember_comment_created
   
   def track_changes?
     updating_user and (status_changed? or assigned_id_changed?)
@@ -80,6 +81,10 @@ class Task < RoleRecord
 
   def assign_to(user)
     self.update_attribute :assigned, user.in_project(project)
+  end
+  
+  def comment_created?
+    !!@comment_created
   end
 
   def overdue
@@ -183,6 +188,10 @@ class Task < RoleRecord
       comment.user = updating_user
     end
     true
+  end
+  
+  def remember_comment_created # before_update
+    @comment_created = comments.any?(&:new_record?)
   end
 
   def save_changes_to_comment # before_save

app/controllers/tasks_controller.rb
@@ -53,13 +53,17 @@ class TasksController < ApplicationController
   def update
     @task.updating_user = current_user
     success = @task.update_attributes params[:task]
-    
+
     respond_to do |f|
       f.html {
         if request.xhr? or iframe?
-          comment = @task.comments.last(:order => 'id')
-          render :partial => 'comments/comment',
-            :locals => { :comment => comment, :threaded => true }
+          if @task.comment_created?
+            comment = @task.comments.last(:order => 'id')
+            render :partial => 'comments/comment',
+              :locals => { :comment => comment, :threaded => true }
+          else
+            render :nothing => true
+          end
         else
           if success
             redirect_to_task

app/javascripts/comment.js
@@ -98,9 +98,11 @@ document.on('ajax:success', '.thread .comments .more_comments', function(e, el) 
 // insert new comment into thread after posting
 document.on('ajax:success', '.thread form', function(e, form) {
   resetCommentsForm(form)
-  form.up('.thread').down('.comments').insert(e.memo.responseText).
-    down('.comment:last-child').highlight({ duration: 1 })
-	format_posted_date()
+  if (!e.memo.responseText.blank()) {
+    form.up('.thread').down('.comments').insert(e.memo.responseText).
+      down('.comment:last-child').highlight({ duration: 1 })
+	  format_posted_date()
+  }
 })
 
 document.on('ajax:failure', 'form.new_conversation, .thread form', function(e, form) {

commit 5815a1fcde9f611c09403d421a96f4abbf1cd98e

fix task saving regression
introduced in fc757af

app/models/task.rb
@@ -185,6 +185,7 @@ class Task < RoleRecord
   
   def remember_comment_created # before_update
     @comment_created = comments.any?(&:new_record?)
+    true
   end
 
   def save_changes_to_comment # before_save