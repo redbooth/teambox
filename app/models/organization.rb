45e7545a9c42a8ada4df196dd5a994be36e0e146
Jordi Romero <jordi@jrom.net>
2010-09-13 16:52:11 +0200

    No membership without organization

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,6 +1,6 @@
 class Organization < ActiveRecord::Base
   has_many :projects #, :dependent => :destroy
-  has_many :memberships
+  has_many :memberships, :dependent => :destroy
 
   has_many :users, :through => :memberships
   has_many :admins, :through => :memberships, :source => :user, :conditions => {'memberships.role' => Membership::ROLES[:admin]}
