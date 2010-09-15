37700033fe25b9959f949e598413a0e3a62afbf6
Jordi Romero <jordi@jrom.net>
2010-09-15 11:20:17 +0200

    Don't check for unique domains when they are blank.

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -10,7 +10,7 @@ class Organization < ActiveRecord::Base
 
   validates_presence_of   :permalink
   validates_uniqueness_of :permalink, :case_sensitive => false
-  validates_uniqueness_of :domain, :case_sensitive => false, :allow_nil => true
+  validates_uniqueness_of :domain, :case_sensitive => false, :allow_nil => true, :allow_blank => true
   validates_length_of     :permalink, :minimum => 4
   validates_exclusion_of  :permalink, :in => %w(www help support mail pop smtp ftp)
   validates_format_of     :permalink, :with => /^[\w\_\-]+$/
