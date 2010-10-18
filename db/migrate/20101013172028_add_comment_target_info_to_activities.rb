f0ba30cedcc72472d89c3197ffdc3bab74753588
Jordi Romero <jordi@jrom.net>
2010-10-18 17:31:56 +0200

    refactor activities migration

--- db/migrate/20101013172028_add_comment_target_info_to_activities.rb
+++ db/migrate/20101013172028_add_comment_target_info_to_activities.rb
@@ -2,11 +2,7 @@ class AddCommentTargetInfoToActivities < ActiveRecord::Migration
   def self.up
     add_column :activities, :comment_target_id, :integer
     rename_column :activities, :comment_type, :comment_target_type
-
-    # This can be slow, act cautiously
-    Activity.find_each(:conditions => { :target_type => 'Comment' }) do |activity|
-      activity.update_attribute :comment_target_id, activity.target.target.id
-    end
+    Activity.update_all "comment_target_id = (select comments.target_id from comments where comments.id = activities.target_id)", "target_type = 'Comment'"
   end
 
   def self.down
