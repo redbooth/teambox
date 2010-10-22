e092b775f6fe09ba9ed9c286f3e2d47026bf6041
Denis Novikov <denis.novikov@freakingloud.com>
2010-10-22 17:15:27 +0200

    Route change for shorthand urls for a single task.

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ ActionController::Routing::Routes.draw do |map|
   map.register          '/register',            :controller => 'users',       :action => 'create'
   map.signup            '/signup',              :controller => 'users',       :action => 'new'
 
-  map.task              '/task/:task_id',       :controller => 'tasks',       :action => 'shorthand'
+  map.tasks             '/tasks/:task_id',      :controller => 'tasks',       :action => 'shorthand'
   
   map.search            '/search',              :controller => 'search'
 
