b22a7ea950ac3511a257eb7c820332aacf93fb21
Pablo Villalba <pablo@teambox.com>
2010-11-02 13:52:15 +0100

    Excluding the "guide" subdomain for organizations

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -15,7 +15,7 @@ class Organization < ActiveRecord::Base
   validates_uniqueness_of :permalink, :case_sensitive => false
   validates_uniqueness_of :domain, :case_sensitive => false, :allow_nil => true, :allow_blank => true
   validates_length_of     :permalink, :minimum => 4
-  validates_exclusion_of  :permalink, :in => %w(www help support mail pop smtp ftp)
+  validates_exclusion_of  :permalink, :in => %w(www help support mail pop smtp ftp guide)
   validates_format_of     :permalink, :with => /^[\w\_\-]+$/
 
   validate :ensure_unicity_for_community_version, :on => :create, :unless => :is_example
