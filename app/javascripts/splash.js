c887b3eb9c5c500a6e5730be1531c894610654a8
Pablo Villalba <pablo@teambox.com>
2010-11-02 20:26:58 +0100

    Referencing container by id (for splash)

--- app/javascripts/splash.js
+++ app/javascripts/splash.js
@@ -3,12 +3,12 @@ document.on("dom:loaded", function() {
     $(document.body).insert({
       after: "<div id='splash'><a id='hide_splash' href='/disable_splash'><img src='/images/splash.jpg'/></a></div>"
     })
-    $$('.container')[0].hide()
+    $('container').hide()
     
     document.on("click", "#hide_splash", function(e,link) {
       Event.stop(e)
       new Ajax.Request(link.href, { method: 'get' })
-      $$('.container')[0].show()
+      $('container').show()
       $('splash').fade()
       window.location.hash = ""
     })
