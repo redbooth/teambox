bda4ef9255744a8ec5e6061d047ae11e5686fee5
Jordi Romero <jordi@jrom.net>
2010-11-07 23:06:58 +0100

    Updating plugin rspec-on-rails-matchers to avoid deprecations

--- vendor/plugins/rspec-on-rails-matchers/lib/spec/rails/matchers/validations.rb
+++ vendor/plugins/rspec-on-rails-matchers/lib/spec/rails/matchers/validations.rb
@@ -1,14 +1,18 @@
 module Spec
   module Rails
     module Matchers
-      def validate_presence_of(attribute)
-        return simple_matcher("model to validate the presence of #{attribute}") do |model|
+
+      Spec::Matchers.define :validate_presence_of do |attribute|
+        match do |model|
           model.send("#{attribute}=", nil)
           !model.valid? && model.errors.invalid?(attribute)
         end
+        description do
+          "model to validate the presence of #{attribute}"
+        end
       end
 
-      def validate_length_of(attribute, options)
+      Spec::Matchers.define :validate_length_of do |attribute, options|
         if options.has_key? :within
           min = options[:within].first
           max = options[:within].last
@@ -20,37 +24,44 @@ module Spec
         elsif options.has_key? :maximum
           max = options[:maximum]
         end
-        
-        return simple_matcher("model to validate the length of #{attribute} within #{min || 0} and #{max || 'Infinity'}") do |model|
+        match do |model|
           invalid = false
           if !min.nil? && min >= 1
             model.send("#{attribute}=", 'a' * (min - 1))
-
             invalid = !model.valid? && model.errors.invalid?(attribute)
           end
-          
+
           if !max.nil?
             model.send("#{attribute}=", 'a' * (max + 1))
-
             invalid ||= !model.valid? && model.errors.invalid?(attribute)
           end
           invalid
         end
+        description do
+          "model to validate the length of #{attribute} within #{min || 0} and #{max || 'Infinity'}"
+        end
       end
 
-      def validate_uniqueness_of(attribute)
-        return simple_matcher("model to validate the uniqueness of #{attribute}") do |model|
+      Spec::Matchers.define :validate_uniqueness_of do |attribute|
+        match do |model|
           model.class.stub!(:find).and_return(true)
           !model.valid? && model.errors.invalid?(attribute)
         end
+        description do
+          "model to validate the uniqueness of #{attribute}"
+        end
       end
 
-      def validate_confirmation_of(attribute)
-        return simple_matcher("model to validate the confirmation of #{attribute}") do |model|
+      Spec::Matchers.define :validate_confirmation_of do |attribute|
+        match do |model|
           model.send("#{attribute}_confirmation=", 'asdf')
           !model.valid? && model.errors.invalid?(attribute)
         end
+        description do
+          "model to validate the confirmation of #{attribute}"
+        end
       end
+
     end
   end
 end
