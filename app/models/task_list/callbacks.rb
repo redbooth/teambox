cf8157b43f2623319ed6128ecaee1f175eec1284
Charles Barbier <unixcharles@gmail.com>
2010-11-08 18:51:10 +0100

    TaskList should alway be in first position on creation

--- app/models/task_list/callbacks.rb
+++ app/models/task_list/callbacks.rb
@@ -1,12 +1,9 @@
 class TaskList
-  def before_save
+  def before_create
     unless self.position
-      first_task_list = self.project.task_lists.first(:select => 'position')
-      if first_task_list
-        last_position = first_task_list.position
-        self.position = last_position.nil? ? 1 : last_position.succ
-      else
-        self.position = 0
+      self.position = 0
+      project.task_lists.each do |t|
+        t.increment!(:position) unless t == self
       end
     end
   end
