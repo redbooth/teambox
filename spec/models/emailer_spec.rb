be65c1ee140e53e5a2574a4cba6892f031f389d7
Charles Barbier <unixcharles@gmail.com>
2010-11-12 18:15:20 +0100

    spec for the emailer render methods, view / locale

--- spec/models/emailer_spec.rb
+++ spec/models/emailer_spec.rb
@@ -27,6 +27,97 @@ describe Emailer do
     end
   end
   
+  describe "email rendering" do
+    # I18n.available_locales, too slow! Top 3 only
+    [:en, :es, :fr].each do |locale|
+      before do
+        @user = Factory(:user, :locale => locale.to_s)
+      end
+
+      it "should render valid task notification for #{locale}" do
+        @task = Factory(:task)
+        Factory(:comment, :target => @task)
+
+        with_locale(locale) do
+          lambda { Emailer.create_notify_task(@user, @task.project, @task) }.should_not raise_error
+        end
+      end
+
+      it "should render valid conversation notification for #{locale}" do
+        @conversation = Factory(:conversation)
+        Factory(:comment, :target => @conversation)
+
+        with_locale(locale) do
+          lambda { Emailer.create_notify_conversation(@user, @conversation.project, @conversation) }.should_not raise_error
+        end
+      end
+
+      it "should render valid daily task reminder for #{locale}" do
+        @task = Factory(:task)
+        @task.assign_to(@user)
+        @task.save
+
+        Factory(:comment, :target => @task, :due_on => Time.now + 1.day)
+
+        with_locale(locale) do
+          lambda { Emailer.create_daily_task_reminder(@user) }.should_not raise_error
+        end
+      end
+
+      it "should render valid signup invitation for #{locale}" do
+        @invitation = Factory(:invitation)
+
+        with_locale(locale) do
+          lambda { Emailer.create_signup_invitation(@invitation) }.should_not raise_error
+        end
+      end
+
+      it "should render valid reset password for #{locale}" do
+        with_locale(locale) do
+          lambda { Emailer.create_reset_password(@user) }.should_not raise_error
+        end
+      end
+
+      it "should render valid forgot password for #{locale}" do
+        @password_reset = Factory(:reset_password)
+
+        with_locale(locale) do
+          lambda { Emailer.create_forgot_password(@password_reset) }.should_not raise_error
+        end
+      end
+
+      it "should render valid project membership notification for #{locale}" do
+        @invitation = Factory(:invitation)
+        @invitation.invited_user = Factory(:user)
+        @invitation.save
+
+        with_locale(locale) do
+          lambda { Emailer.create_project_membership_notification(@invitation) }.should_not raise_error
+        end
+      end
+
+      it "should render valid project invitation for #{locale}" do
+        @invitation = Factory(:invitation)
+        with_locale(locale) do
+          lambda { Emailer.create_project_invitation(@invitation) }.should_not raise_error
+        end
+      end
+
+      it "should render valid email confirmation for #{locale}" do
+        with_locale(locale) do
+          lambda { Emailer.create_confirm_email(@user) }.should_not raise_error
+        end
+      end
+    end
+  end
+
+  def with_locale(language)
+    old_locale = I18n.locale
+    I18n.locale = language
+    yield
+    I18n.locale = old_locale
+  end
+
   def allow_incoming_mail(really = true)
     old_value = Teambox.config.allow_incoming_email
     Teambox.config.allow_incoming_email = really
