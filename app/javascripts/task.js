f6582f380553b86bcb03b0badc2dbd9bb0f0deaf
Pablo Villalba <pablo@teambox.com>
2010-12-15 16:51:38 +0100

    Sometimes there's no task counter, fixing js bug

--- app/javascripts/task.js
+++ app/javascripts/task.js
@@ -32,9 +32,9 @@ document.on('ajax:success', '.task_header + form.edit_task', function(e, form) {
 
 // update task counter
 document.on('ajax:success', 'form.edit_task', function(e, form) {
-	var task_data = e.memo.headerJSON
-	counter = $$('.task_counter[data-task-id='+ task_data.id +']').first()
-  counter.update(parseInt(counter.innerHTML) + 1)
+  var task_data = e.memo.headerJSON
+  counter = $$('.task_counter[data-task-id='+ task_data.id +']').first()
+  if (counter) counter.update(parseInt(counter.innerHTML) + 1)
 })
 
 document.on('click', '.date_picker', function(e, element) {
