398ab4e5e075013cbf0d87de3dedd944971c9464
Saimon Moore <saimon@saimonmoore.net>
2010-12-17 16:38:29 +0100

    Reworking routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Teambox::Application.routes.draw do
   resources :sprockets, :only => [:index, :show]
+
   match '/logout' => 'sessions#destroy', :as => :logout
   match '/login' => 'sessions#new', :as => :login
   match '/login/:username' => 'sessions#backdoor', :as => :login_backdoor if Rails.env.cucumber?
+
   match '/register' => 'users#create', :as => :register
   match '/signup' => 'users#new', :as => :signup
+
   match '/search' => 'search#index', :as => :search
+
   match '/text_styles' => 'users#text_styles', :as => :text_styles
   match '/invite_format' => 'invitations#invite_format', :as => :invite_format
   match '/feeds' => 'users#feeds', :as => :feeds
@@ -15,26 +19,34 @@ Teambox::Application.routes.draw do
   match '/reset/:reset_code' => 'reset_passwords#reset', :as => :reset_password
   match '/forgetting' => 'reset_passwords#update_after_forgetting', :as => :update_after_forgetting, :method => :put
   match '/reset_password_sent' => 'reset_passwords#sent', :as => :sent_password
+
   match '/format/:f' => 'sessions#change_format', :as => :change_format
+
   match '/projects/:project_id/invite/:login' => 'invitations#create', :as => :create_project_invitation, :method => :post
+
   match '/auth/:provider/callback' => 'auth#callback', :as => :auth_callback
   match '/auth/failure' => 'auth#failure', :as => :auth_failure
   match '/complete_signup' => 'users#complete_signup', :as => :complete_signup
   match '/auth/:provider/unlink' => 'users#unlink_app', :as => :unlink_app
-  match '/auth/google' => 'auth#index', :as => :authorize_google_docs, :provider => 'google'
-  match '/i18n/environment.js' => 'javascripts#environment', :as => :javascript_environment
+  match '/auth/google' => 'auth#index', :as => :authorize_google_docs, :defaults => {:provider => 'google'}
 
   resources :google_docs do
     get :search, :on => :collection
   end
 
-  # Useless resource?
-  # resources :reset_passwords
+  match '/i18n/environment.js' => 'javascripts#environment', :as => :javascript_environment
 
+  #RAILS 3 Useless resource?
+  resources :reset_passwords
   resource :session
-  resources :sites, :only => [:show, :new, :create]
 
   resources :organizations do
+    member do
+      get :projects
+      get :external_view
+      get :delete
+      get :appearance
+    end
     resources :memberships do
       member do
         get :change_role
@@ -42,13 +54,9 @@ Teambox::Application.routes.draw do
         get :remove
       end
     end
-    member do
-      get :projects
-      get :external_view
-      get :delete
-      get :appearance
-    end
   end
+
+  resources :sites, :only => [:show, :new, :create]
 
   match '/account/settings' => 'users#edit', :as => :account_settings, :sub_action => 'settings'
   match '/account/picture' => 'users#edit', :as => :account_picture, :sub_action => 'picture'
@@ -57,10 +65,11 @@ Teambox::Application.routes.draw do
   match '/account/notifications' => 'users#edit', :as => :account_notifications, :sub_action => 'notifications'
   match '/account/delete' => 'users#edit', :as => :account_delete, :sub_action => 'delete'
   match '/account/destroy' => 'users#destroy', :as => :destroy_user
-  resources :teambox_datas
+
+  resources :teambox_datas, :path => '/datas'
 
   resources :users do
-    #RAILS3 - The routes upgrade helper does a terrible job! Revise all routes!
+    resources :invitations
     member do
       get :confirm_email
       get :unconfirmed_email
@@ -77,11 +86,15 @@ Teambox::Application.routes.draw do
   match 'activities/:id/show_new.:format' => 'activities#show_new', :as => :show_new, :method => :get
   match 'activities/:id/show_more.:format' => 'activities#show_more', :as => :show_more, :method => :get
   match 'activities/:id/show_thread.:format' => 'activities#show_thread', :as => :show_thread, :method => :get
+
   match 'projects/archived.:format' => 'projects#index', :as => :project_archived, :sub_action => 'archived'
+
   match 'hooks/:hook_name' => 'hooks#create', :as => :hooks, :via => :post
 
   resources :projects do
     member do
+      post :accept
+      post :decline
       put :transfer
       get :join
     end
@@ -109,12 +122,13 @@ Teambox::Application.routes.draw do
     match 'hooks/:hook_name' => 'hooks#create', :as => :hooks, :via => :post
 
     resources :tasks do
-      resources :comments
       member do
         put :reorder
         put :watch
         put :unwatch
       end
+
+      resources :comments
     end
 
     resources :task_lists do
@@ -131,11 +145,12 @@ Teambox::Application.routes.draw do
       end
 
       resources :tasks do
-        resources :comments
         member do
           put :watch
           put :unwatch
         end
+
+        resources :comments
       end
     end
 
@@ -148,12 +163,13 @@ Teambox::Application.routes.draw do
     end
 
     resources :conversations do
-      resources :comments
       member do
         put :convert_to_task
         put :watch
         put :unwatch
       end
+
+      resources :comments
     end
 
     resources :pages do
@@ -167,13 +183,16 @@ Teambox::Application.routes.draw do
       resources :notes,:dividers,:uploads
     end
 
+    match 'search' => 'search#index', :as => :search
+
     resources :google_docs do
-      get :search, :on => :collection
+      collection do
+        :search
+      end
     end
   end
 
   match '/public' => 'public/projects#index', :as => :public_projects
-  root :to => 'projects#index'
 
   namespace :public do
     match ':id' => 'projects#show', :as => :project
@@ -188,84 +207,136 @@ Teambox::Application.routes.draw do
   match 'api/changes' => 'apidocs#changes', :as => :api_changes
   match 'api/:model' => 'apidocs#model', :as => :api_model
 
-  scope 'api/1' do
-    namespace :api_v1 do
-      resources :projects do
-        resources :activities
-        resources :people
-        resources :comments
-        resources :conversations do
-          resources :comments
-        end
-        resources :invitations do
-          member do
-            put :resend
-          end
-        end
-        resources :task_lists do
-          resources :tasks
-        end
-        resources :tasks do
-          resources :comments
-        end
-        resources :uploads
-        resources :pages do
-          collection do
-            put :resort
-          end
-          member do
-            put :reorder
-          end
-        end
-        resources :notes
-        resources :dividers
-        match 'search' => 'search#index', :as => :search
+  namespace :api_v1, :path => 'api/1' do
+    resources :projects, :except => [:new, :edit] do
+      member do
+        put :transfer
       end
-      resources :activities
-      resources :invitations do
-        member do
-          put :accept
-        end
-      end
-      resources :users
-      resources :tasks do
+
+      resources :activities, :only => [:index, :show]
+
+      resources :people, :except => [:create, :new, :edit]
+
+      resources :comments, :except => [:new, :create, :edit]
+
+      resources :conversations, :except => [:new, :edit] do
         member do
           put :watch
           put :unwatch
         end
+
+        resources :comments, :except => [:new, :edit]
       end
-      resources :comments
-      resources :conversations do
-        resources :comments
+
+      resources :invitations, :except => [:new, :edit, :update] do
+        member do
+          put :resend
+        end
       end
-      resources :task_lists do
-        resources :tasks
+
+      resources :task_lists, :except => [:new, :edit] do
+        member do
+          put :archive
+          put :unarchive
+        end
+
+        resources :tasks, :except => [:new, :edit]
       end
-      resources :tasks do
-        resources :comments
+
+      resources :tasks, :except => [:new, :edit, :create] do
+        member do
+          put :watch
+          put :unwatch
+        end
+
+        resources :comments, :except => [:new, :edit]
       end
-      resources :uploads
-      resources :pages do
+
+      resources :uploads, :except => [:new, :edit, :update]
+
+      resources :pages, :except => [:new, :edit] do
         collection do
           put :resort
         end
+
         member do
           put :reorder
         end
       end
-      resources :notes
-      resources :dividers
-      resources :organizations do
-        resources :projects do
-          member do
-            put :transfer
-          end
-        end
-        resources :memberships
-      end
+
+      resources :notes, :except => [:new, :edit]
+
+      resources :dividers, :except => [:new, :edit]
+
       match 'search' => 'search#index', :as => :search
-      match 'account' => 'users#current', :as => :account, :via => :get
     end
+
+    resources :activities, :only => [:index, :show]
+
+    resources :invitations, :except => [:new, :edit, :update, :create] do
+      member do
+        put :accept
+      end
+    end
+
+    resources :users, :only => [:index, :show]
+
+    resources :tasks, :except => [:new, :edit, :create] do
+      member do
+        put :watch
+        put :unwatch
+      end
+    end
+
+    resources :comments, :except => [:new, :create, :edit]
+
+    resources :conversations, :except => [:new, :edit] do
+      member do
+        put :watch
+        put :unwatch
+      end
+
+      resources :comments, :except => [:new, :edit]
+    end
+
+    resources :task_lists, :except => [:new, :edit] do
+      resources :tasks, :except => [:new, :edit]
+    end
+
+    resources :tasks, :except => [:new, :edit, :create] do
+      member do
+        put :watch
+        put :unwatch
+      end
+
+      resources :comments, :except => [:new, :edit]
+    end
+
+    resources :uploads, :except => [:new, :edit, :update]
+    resources :pages, :except => [:new, :edit] do
+      collection do
+        put :resort
+      end
+      member do
+        put :reorder
+      end
+    end
+
+    resources :notes, :except => [:new, :edit]
+
+    resources :dividers, :except => [:new, :edit]
+
+    resources :organizations, :except => [:new, :edit, :destroy] do
+      resources :projects, :except => [:new, :edit] do
+        member do
+          put :transfer
+        end
+      end
+
+      resources :memberships, :except => [:new, :edit, :create]
+    end
+    match 'search' => 'search#index', :as => :search
+    match 'account' => 'users#current', :as => :account, :via => :get
   end
 
   resources :task_lists, :only => [ :index ] do
@@ -275,13 +346,17 @@ Teambox::Application.routes.draw do
   end
 
   resources :conversations, :only => [ :create ]
+
   match 'time/:year/:month' => 'hours#index', :as => :hours_by_month, :via => :get
   match 'time/by_period' => 'hours#by_period', :as => :hours_by_period, :via => :get
   match 'time' => 'hours#index', :as => :time
-  match '/' => 'projects#index'
+
+  root :to => 'projects#index'
+
   match 'assets/:id/:style/:filename' => 'uploads#download', :constraints => { :filename => /.*/ }, :via => :get
 
   if Rails.env.development?
     mount Emailer::Preview => 'mail_view'
   end
+
 end
