29cc90ea2bb695ce4cf161e8d21186db7c46b27c
Jordi Romero <jordi@jrom.net>
2010-12-29 12:16:04 +0100

    Permalink validations shouldn't consider deleted items

--- app/models/project/validation.rb
+++ app/models/project/validation.rb
@@ -3,7 +3,7 @@ class Project
   validates_length_of :name, :minimum => 5, :on => :create  # New projects
   validates_length_of :name, :minimum => 5, :on => :update, :if => :name_changed?  # Changing the name
   validates_length_of :name, :minimum => 3, :on => :update  # Legacy validation for existing projects
-  validates_uniqueness_of :permalink, :case_sensitive => false
+  validates_uniqueness_of :permalink, :case_sensitive => false, :scope => :deleted
   validates_length_of :permalink, :minimum => 5
   validates_format_of :permalink, :with => /^[a-z0-9_\-]{5,}$/, :if => :permalink_length_valid?
 
