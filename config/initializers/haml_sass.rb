f3274cb515187bd90cf15d1f3d4003ae4cc689cf
Jordi Romero <jordi@jrom.net>
2011-01-06 19:09:55 +0100

    Fix serving compiled Sass from Heroku
    We need to plug the Rack::Static (and then the Sass::Plugin::Rack)
    middleware earlier in heroku.

--- config/initializers/haml_sass.rb
+++ config/initializers/haml_sass.rb
@@ -12,6 +12,8 @@ Sass::Plugin.add_template_location(Rails.root + 'app/styles', Rails.root + css_d
 
 if Teambox.config.heroku?
   # add Rack middleware to serve compiled stylesheets from "tmp/stylesheets"
+  Teambox::Application.config.middleware.delete 'Sass::Plugin::Rack'
+  Teambox::Application.config.middleware.insert_after 'ActionDispatch::Static', 'Sass::Plugin::Rack'
   Teambox::Application.config.middleware.insert_after 'Sass::Plugin::Rack', 'Rack::Static',
     :urls => ['/stylesheets'], :root => "#{Rails.root}/tmp"
 end
