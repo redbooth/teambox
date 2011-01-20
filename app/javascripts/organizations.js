cc41f3c965506b39132c0d82a03a104081271667
Charles Barbier <unixcharles@gmail.com>
2011-01-20 12:36:03 -0500

    fix a warning with org/js

--- app/javascripts/organizations.js
+++ app/javascripts/organizations.js
@@ -4,10 +4,10 @@ document.observe("dom:loaded", function() {
   colour_settings.each(function(setting) {
     var fieldName = "organization_settings_colours_" + setting,
         swatchName = fieldName + '_swatch',
-        field = $(fieldName),
-				defaultColor = field.readAttribute('data-default-color')
+        field = $(fieldName)
 
     if (field && window.Control && window.Control.ColorPicker) {
+			defaultColor = field.readAttribute('data-default-color')
       field.colorPicker = new Control.ColorPicker(fieldName, { IMAGE_BASE : "/images/colorPicker/", 'swatch': swatchName, 'defaultColor': defaultColor})
     }
   });
