e9b0fbd554360b07e4b1e7547fe92813fecd5642
Charles Barbier <unixcharles@gmail.com>
2011-01-27 10:46:56 -0500

    syntax error, index migration

--- db/migrate/20110125200118_add_uniqueness_index_to_people.rb
+++ db/migrate/20110125200118_add_uniqueness_index_to_people.rb
@@ -1,11 +1,11 @@
 class AddUniquenessIndexToPeople < ActiveRecord::Migration
   def self.up
-    remove_index :people, [:user_id, :project_id], :name => "index_people_on_user_id_and_project_id"
-    add_index :people, [:user_id, :project_id], :name => "index_people_on_user_id_and_project_id", :unique => true
+    remove_index :people, [:user_id, :project_id]
+    add_index :people, [:user_id, :project_id], :unique => true
   end
 
   def self.down
-    remove_index :people, [:user_id, :project_id], :name => "index_people_on_user_id_and_project_id"
-    add_index :people, [:user_id, :project_id], :name => "index_people_on_user_id_and_project_id"
+    remove_index :people, [:user_id, :project_id]
+    add_index :people, [:user_id, :project_id]
   end
 end
