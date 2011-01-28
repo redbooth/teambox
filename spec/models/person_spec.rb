6eb6883862b85dd6c6a285420c4fa0961052c9da
Enric and Jordi <jordi+enric@jrom.net>
2011-01-28 12:20:14 +0100

    spec for basic Person creation

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -27,4 +27,14 @@ describe Person do
     project.add_user(user)
     project.reload.people.map(&:id).include?(person_id).should == true
   end
+
+  it "should be created" do
+    @user = Factory(:user)
+    @project1 = Factory(:project)
+    @project2 = Factory(:project)
+    @user.projects(true).should be_empty
+    @project1.add_user(@user)
+    @project2.add_user(@user)
+    @user.projects(true).should include(@project1, @project2)
+  end
 end
