ff63fe4f65c727829b1c8045d3a023ed79c1f4f8
Enric Lluelles <enriclluelles@gmail.com>
2011-01-31 17:13:51 +0100

    Adding spec to test translations

---
+++ spec/helpers/translation_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe 'Translations' do
+
+  before :all do
+    I18n.backend.store_translations :en, :test_number => "Test number %{number}"
+    I18n.locale=:ca
+  end
+
+  it "should fall back to the default locale when the key doesn't exist" do
+    I18n.t(:test_number, :number=>1).should == "Test number 1"
+  end
+
+  it "should fall back to the default locale when the interpolation doesn't work" do
+    I18n.backend.store_translations :ca, :test_number => "Test número %{numero}"
+    I18n.t(:test_number, :number=>1).should == "Test number 1"
+  end
+
+  it "should translate normally when everything is alright" do
+    I18n.backend.store_translations :ca, :test_number => "Test número %{number}"
+    I18n.t(:test_number, :number=>1).should == "Test número 1"
+  end
+
+end
