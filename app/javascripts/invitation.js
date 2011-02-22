cee1422d94b618ee0d346747e60e734b3fd91b61
Enric Lluelles <enriclluelles@gmail.com>
2011-02-22 17:15:22 +0100

    Showing select with the locales for invitations

--- app/javascripts/invitation.js
+++ app/javascripts/invitation.js
@@ -1,3 +1,8 @@
 document.on('ajax:success', 'a.invitation-destroy', function(e,link) {
 	link.up('.invitation').fade()
 })
+
+document.on('click','#invitation_select_language', function(e) {
+  e.element().replace($("all_locales").innerHTML)
+  $('all_locales').remove()
+})
