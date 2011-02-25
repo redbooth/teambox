403532494406cfa076f3c31f4422cd4a6e51e9c5
Jordi Romero <jordi@jrom.net>
2011-02-25 11:17:26 +0100

    fix uploads_count migration for postgres and mysql

--- db/migrate/20101117103529_add_uploads_count_to_comments.rb
+++ db/migrate/20101117103529_add_uploads_count_to_comments.rb
@@ -1,7 +1,12 @@
 class AddUploadsCountToComments < ActiveRecord::Migration
   def self.up
     add_column :comments, :uploads_count, :integer, :default => 0
-    Comment.update_all "uploads_count = (SELECT COUNT(*) FROM `uploads` WHERE uploads.comment_id = comments.id AND uploads.deleted_at IS NULL)", "deleted_at IS NULL"
+    case ActiveRecord::Base.configurations[Rails.env]['adapter']
+    when 'postgresql'
+      Comment.update_all "uploads_count = (SELECT COUNT(*) FROM uploads,comments WHERE uploads.comment_id = comments.id AND uploads.deleted_at IS NULL)", "deleted_at IS NULL"
+    else
+      Comment.update_all "uploads_count = (SELECT COUNT(*) FROM uploads WHERE uploads.comment_id = comments.id AND uploads.deleted_at IS NULL)", "deleted_at IS NULL"
+    end
   end
 
   def self.down
