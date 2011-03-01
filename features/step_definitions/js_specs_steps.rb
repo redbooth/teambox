6079a594c7733f7f5843a2112b6e8e47df2ac85a
Jordi Romero <jordi@jrom.net>
2011-03-01 14:22:49 +0100

    fix false positive js specs cuke

--- features/step_definitions/js_specs_steps.rb
+++ features/step_definitions/js_specs_steps.rb
@@ -1,7 +1,8 @@
-When /^I run the (.+?) specs$/ do |suite|
+When /^I run the (.+?) javascript specs$/ do |suite|
   visit "/js_specs/#{suite}" 
 end
 
 Then /^I should see all specs passing$/ do
+  Then %(I should see "Test passed")
   Then %(I should not see "FAILED")
 end
