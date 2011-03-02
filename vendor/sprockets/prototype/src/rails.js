b2384281d3992cb3b924549566609732e162561b
Enric Lluelles <enriclluelles@gmail.com>
2011-03-02 12:04:49 +0100

    Adding the csrf headers when sending ajax requests

--- vendor/sprockets/prototype/src/rails.js
+++ vendor/sprockets/prototype/src/rails.js
@@ -1,4 +1,14 @@
 (function() {
+  Ajax.Responders.register({
+    onCreate: function(request) {
+      var token = $$('meta[name=csrf-token]')[0];
+      if (token) {
+        if (!request.options.requestHeaders) request.options.requestHeaders = {};
+        request.options.requestHeaders['X-CSRF-Token'] = token.readAttribute('content');
+      }
+    }
+  });
+
   // Technique from Juriy Zaytsev
   // http://thinkweb2.com/projects/prototype/detecting-event-support-without-browser-sniffing/
   function isEventSupported(eventName) {
@@ -126,7 +136,7 @@
         csrf_token = $$('meta[name=csrf-token]')[0];
 
     var form = new Element('form', { method: "POST", action: url, style: "display: none;" });
-    element.parentNode.insert(form);
+    $(element.parentNode).insert(form);
 
     if (method !== 'post') {
       insertHiddenField(form, '_method', method);
