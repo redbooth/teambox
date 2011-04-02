0cb71268fa7d8e9b5794bfc09bf919e22d6d52be
Charles Barbier <unixcharles@gmail.com>
2011-04-01 18:54:56 -0700

    Add scenario, send reminder in user locale

--- features/step_definitions/task_reminders_steps.rb
+++ features/step_definitions/task_reminders_steps.rb
@@ -5,3 +5,12 @@ end
 When "the daily task reminders go out" do
   User.send_daily_task_reminders
 end
+
+Then /I change my locale to (\w+)/ do |locale|
+  new_locale = case locale
+  when 'français' then :fr
+  end
+
+  @current_user.locale = new_locale
+  @current_user.save
+end
