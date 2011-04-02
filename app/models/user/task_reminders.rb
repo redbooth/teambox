e078136418681928d0072bcdee6fb0cdbc310a13
Charles Barbier <unixcharles@gmail.com>
2011-04-01 18:55:54 -0700

    Task reminder email should be send in user locale

--- app/models/user/task_reminders.rb
+++ app/models/user/task_reminders.rb
@@ -12,7 +12,7 @@ class User
     
     self.wants_task_reminder_email.in_time_zone(zones.map(&:name)).find_each do |user|
       if user.assigned_tasks.due_sooner_than_two_weeks.any?
-        Emailer.send_email :daily_task_reminder, user.id
+        Emailer.send_with_language :daily_task_reminder, user.locale.to_sym, user.id
       end
     end
   end
