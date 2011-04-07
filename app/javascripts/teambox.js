21dc1c7c96336d01d0383fcd4b79d664e32827b1
Jordi Romero <jordi@jrom.net>
2011-04-07 10:43:01 -0700

    refactor Teambox initialization

--- app/javascripts/teambox.js
+++ app/javascripts/teambox.js
@@ -2,6 +2,37 @@ Teambox = {
   Models: {},
   Collections: {},
   Controllers: {},
-  Views: {}
+  Views: {},
+  init: function() {
+    // Set the new root url
+    if (window.location.hash === '') {
+      window.location.hash = '#!/';
+    }
+
+    // Fetch current user
+    Teambox.my_user = new Teambox.Models.User();
+    Teambox.my_user.fetch();
+
+    // Fetch my tasks
+    Teambox.my_tasks = new Teambox.Collections.Tasks();
+    Teambox.my_tasks.fetch();
+
+    // Print my tasks
+    Teambox.my_tasks_counter_view = new Teambox.Views.MyTasksCounter({
+      collection: Teambox.my_tasks
+    });
+
+    Teambox.my_tasks_view = new Teambox.Views.MyTasks({
+      collection: Teambox.my_tasks
+    });
+
+    Teambox.all_tasks_view = new Teambox.Views.AllTasks({
+      collection: Teambox.my_tasks
+    });
+
+    app = new Teambox.Controllers.AppController();
+    Backbone.history.start();
+
+  }
 };
 
