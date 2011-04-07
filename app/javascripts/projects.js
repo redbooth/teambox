e1c958df245062a8b919e2aa98981ba5ad7aba1c
James Urquhart <jamesu@gmail.com>
2011-04-07 21:35:35 +0100

    Use facebox for confirming project deletion

--- app/javascripts/projects.js
+++ app/javascripts/projects.js
@@ -50,3 +50,14 @@ document.on('click', 'a.show_archived', function(e,el) {
   el.hide()
   el.up().next('.archived_projects').appear({duration: 0.2})
 })
+
+document.on('click', 'a.delete_project', function(e, el){
+	e.preventDefault()
+	Prototype.Facebox.open($('delete_project_html').innerHTML, 'html delete_project_box', {
+		buttons: [
+			{className: 'confirm', href:el.readAttribute('href'), description: I18n.translations.projects.fields.delete_this_project,
+			 extra:"data-method='delete'"},
+			{className: 'close', href:'#close', description: I18n.translations.common.cancel}
+		]
+	})
+})
