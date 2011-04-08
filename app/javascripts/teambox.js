845bdb2522b05a07ac602d2d5336f7f9a9390312
Pablo Villalba <pablo@teambox.com>
2011-04-08 13:44:08 -0500

    Loading methods

--- app/javascripts/teambox.js
+++ app/javascripts/teambox.js
@@ -9,17 +9,21 @@ Teambox = {
       window.location.hash = '#!/';
     }
 
+    var loaded = function(req) {
+      return function() { console.log("Loaded "+req); };
+    };
+
     // Fetch current user
     Teambox.my_user = new Teambox.Models.User();
-    Teambox.my_user.fetch();
+    Teambox.my_user.fetch({ success: loaded('user') });
 
     // Fetch my tasks
     Teambox.my_tasks = new Teambox.Collections.Tasks();
-    Teambox.my_tasks.fetch();
+    Teambox.my_tasks.fetch({ success: loaded('tasks') });
 
     // Fetch my threads
     Teambox.my_threads = new Teambox.Collections.Threads();
-    Teambox.my_threads.fetch();
+    Teambox.my_threads.fetch({ success: loaded('activities') });
 
     // Print my tasks
     Teambox.my_tasks_counter_view = new Teambox.Views.MyTasksCounter({
