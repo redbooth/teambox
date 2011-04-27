db9f327458b789c922f9c031b3a29a69eaed67cd
James Urquhart <jamesu@gmail.com>
2011-04-27 17:41:12 +0100

    Better handling of state for the private element selection

--- app/javascripts/private_elements.js
+++ app/javascripts/private_elements.js
@@ -3,7 +3,16 @@ PrivateBox = {
   redrawBox: function(form, project_id) {
     var box = form.down('.private_options');
     box.select('.private_users').invoke('remove');
-    box.insert({ bottom: this.peopleHTML(project_id) });
+
+    if (box.down('.option.private input').checked)
+      box.insert({ bottom: this.peopleHTML(project_id) });
+  },
+
+  update: function(el) {
+    var form = el.up('form');
+    var select = $$('select#project_id');
+    var project_id = form.readAttribute('data-project-id') || select[0].select('option').find(function(ele){return !!ele.selected;}).value;
+    PrivateBox.redrawBox(form, project_id);
   },
 
   peopleHTML: function(project_id) {
@@ -22,24 +31,18 @@ PrivateBox = {
 
 document.on("click", "a.private_switch", function(e,el) {
   e.stop();
+  PrivateBox.update(el);
   el.up('form').down('.private_options').toggle();
 });
 
-document.on("click", ".private_options .option.normal", function(e,el) {
-  var box = el.up('form').down('.private_options');
-  box.select('.private_users').invoke('remove');
+document.on("change", ".private_options .option.normal", function(e,el) {
+  PrivateBox.update(el);
 });
 
-document.on("click", ".private_options .option.private", function(e,el) {
-  var form = el.up('form');
-  var select = form.down('select#project_id');
-  var project_id = form.readAttribute('data-project-id') || select.select('option').find(function(ele){return !!ele.selected;}).value;
-  PrivateBox.redrawBox(form, project_id);
+document.on("change", ".private_options .option.private", function(e,el) {
+  PrivateBox.update(el);
 });
 
 document.on("change", "select#project_id", function(e,el) {
-  var select = el;
-  var form = el.up("form");
-  var project_id = select.select('option').find(function(ele){return !!ele.selected;}).value;
-  PrivateBox.redrawBox(form, project_id);
+  PrivateBox.update(el);
 });
