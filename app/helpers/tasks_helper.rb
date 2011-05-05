redbooth/teambox

commit ac9195bdfdb942ec645a46ec24d5609ec3afb26f

Fix, calendar helpers don't take options for now

app/views/comments/_fields.haml
@@ -63,7 +63,7 @@
               = conversation_form.select :status, task_statuses_for_select, {}, :disabled => true
               = conversation_form.select :assigned_id, [], {}, :'data-assigned' => conversation_form.object.assigned_id, :disabled => true
             .fields
-              = date_picker(conversation_form, :due_on, false, :disabled => true)
+              = date_picker(conversation_form, :due_on)
           .submit
             = link_to t('common.cancel'), '#cancel_conversion', :class => 'cancel'
             = f.submit t('comments.convert_form.submit'), :'data-disable-with' => t('comments.new.sending'), :disabled => true

commit c240e887f18f427d9dfc1587af52f6d2e478fc80

Enable html_options for datepicker helper.
Make conversation_due_on in convert-to-task disabled by default.
Fixes: https://teambox.com/projects/teambox-dev/tasks/314777

This is a regression caused by: https://github.com/teambox/teambox/commit/ac9195bdfdb942ec645a46ec24d5609ec3afb26f

app/helpers/tasks_helper.rb
@@ -116,13 +116,13 @@ module TasksHelper
     link_to(t('projects.fields.new.time_tracking_docs'), "http://help.teambox.com/faqs/advanced-features/time-tracking", :target => '_blank')
   end
 
-  def date_picker(f, field, options = {})
+  def date_picker(f, field, options = {}, html_options = {})
     selected_date = f.object.send(field.to_sym) ? localize(f.object.send(field.to_sym), :format => :long) : ''
 
     content_tag :div, :class => "date_picker" do
       [ image_tag('/images/calendar_date_select/calendar.gif', :class => :calendar_date_select_popup_icon),
         content_tag(:span, selected_date.blank? ? t('date_picker.no_date_assigned') : selected_date, :class => 'localized_date'),
-        f.hidden_field(field, :class => :datepicker)
+        f.hidden_field(field, html_options.reverse_merge!(:class => :datepicker))
       ].join.html_safe
     end
   end

app/views/comments/_fields.haml
@@ -63,7 +63,7 @@
               = conversation_form.select :status, task_statuses_for_select, {}, :disabled => true
               = conversation_form.select :assigned_id, [], {}, :'data-assigned' => conversation_form.object.assigned_id, :disabled => true
             .fields
-              = date_picker(conversation_form, :due_on)
+              = date_picker(conversation_form, :due_on, {}, :disabled => true)
           .submit
             = link_to t('common.cancel'), '#cancel_conversion', :class => 'cancel'
             = f.submit t('comments.convert_form.submit'), :'data-disable-with' => t('comments.new.sending'), :disabled => true