7881e7d42dabb419c2a1d80dbcb4c71340d54dd5
Jordi Romero <jordi@jrom.net>
2011-05-16 12:10:23 -0700

    bump private migration name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110418210553) do
+ActiveRecord::Schema.define(:version => 20110516120900) do
 
   create_table "activities", :force => true do |t|
     t.integer  "user_id"
