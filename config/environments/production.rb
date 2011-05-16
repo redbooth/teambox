fdebfbf6ed001dea78f0f1ee3f9219a84d4b02aa
Jordi Romero <jordi@jrom.net>
2011-05-16 16:38:30 -0700

    recover a sane log level in production
    Should only be changed to debug manually when tracking down some issue,
    otherwise it's too verbose.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,7 +34,7 @@ Teambox::Application.configure do
   # just comment this out and Rails will serve the files
 
   # See everything in the log (default is :info)
-  config.log_level = :debug
+  #config.log_level = :debug
 
   # Use a different logger for distributed setups
   # config.logger = SyslogLogger.new
