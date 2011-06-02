0df75c65fa2145db395deebbd5bdb9d04340c075
Enric Lluelles <enriclluelles@gmail.com>
2011-06-02 11:12:54 +0200

    Making an irreversible migration not irreversible

--- db/migrate/20110517003032_transition_from_map_data_to_user_map_and_organization_id.rb
+++ db/migrate/20110517003032_transition_from_map_data_to_user_map_and_organization_id.rb
@@ -7,15 +7,26 @@ class TransitionFromMapDataToUserMapAndOrganizationId < ActiveRecord::Migration
 
   def self.up
     TeamboxData.all.each do |td|
-      td.user_map = td.map_data['User']
-      td.organization = Organization.find_by_permalink(td.map_data['target_organization'])
-      td.save
+      if td.map_data
+        td.user_map = td.map_data['User']
+        td.organization = Organization.find_by_permalink(td.map_data['target_organization'])
+        td.save
+      end
     end
 
     remove_column :teambox_datas, :map_data
   end
 
   def self.down
-    raise ActiveRecord::IrreversibleMigration
+    add_column :teambox_datas, :map_data, :text
+
+    TeamboxData.reset_column_information
+
+    TeamboxData.all.each do |td|
+      td.map_data['User'] = td.user_map if td.user_map
+      td.map_data['target_organization'] = td.organization.permalink if td.organization
+      td.save
+    end
+
   end
 end
