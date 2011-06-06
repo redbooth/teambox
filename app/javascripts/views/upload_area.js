914b8e38ff253738f2d4d28cd1f550bf27ae4db4
Pau Ramon <masylum@gmail.com>
2011-06-06 23:57:22 +0200

    fixed upload params

--- app/javascripts/views/upload_area.js
+++ app/javascripts/views/upload_area.js
@@ -45,14 +45,14 @@
    */
   UploadArea.insertNewUpload = function (evt) {
 
-    //function incrementLastNumber(str) {
-    //  var i = 0, matches = str.match(/\d+/g);
+    function incrementLastNumber(str) {
+      var i = 0, matches = str.match(/\d+/g);
 
-    //  matches.push(parseInt(matches.pop(), 10) + 1);
-    //  return str.gsub(/\d+/, function (m) {
-    //    return matches[i++];
-    //  });
-    //}
+      matches.push(parseInt(matches.pop(), 10) + 1);
+      return str.gsub(/\d+/, function (m) {
+        return matches[i++];
+      });
+    }
 
     var el = evt.element()
       , new_input = new Element('input', {type: 'file', name: el.name /*incrementLastNumber(el.name)*/});
