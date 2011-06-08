02427a2daf8f7691e3e1674b63501f1916d0d40e
Saimon Moore <saimon@saimonmoore.net>
2011-06-08 17:57:05 +0200

    Ignore comments_attributes in js model #toJSON()
    This allows us to use nested models with the api

--- app/javascripts/models/base.js
+++ app/javascripts/models/base.js
@@ -4,9 +4,11 @@ Teambox.Models.Base = Backbone.Model.extend({
   },
   toJSON: function() {
     return _.reduce(this.attributes, function(attrs, v, k) {
-     if (_.all(['object', 'function'], function(type) {return typeof v != type})) {
-       attrs[k]= v;
-     }
+      var checkType = function(type) {
+        return (k !== 'comments_attributes') ? typeof v != type : true;
+      };
+
+     if (_.all(['object', 'function'], checkType)) { attrs[k]= v; }
      return attrs;
     }, {});
   }
