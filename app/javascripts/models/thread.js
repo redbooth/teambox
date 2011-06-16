c95a136bbebbd9ea5b40df28b3578c5eebf68e40
Pau Ramon <masylum@gmail.com>
2011-06-16 14:26:40 +0200

    refactored and jaded search

--- app/javascripts/models/thread.js
+++ app/javascripts/models/thread.js
@@ -1,80 +1,91 @@
 (function () {
 
-  Teambox.Models.Thread = Teambox.Models.Base.extend({
+  var Thread = {};
 
-   /* Returns the class name
-    *
-    * @return {String}
-    */
-    className: function () {
-      return this.get('type');
+ /* Returns the class name
+  *
+  * @return {String}
+  */
+  Thread.className = function () {
+    return this.get('type');
+  };
+
+  /* Checks if the thread is a Task
+   *
+   * @return {Boolean}
+   */
+  Thread.isTask = function () {
+    return this.get('type') === 'Task';
+  };
+
+  /* Checks if the thread is a Conversation
+   *
+   * @return {Boolean}
+   */
+  Thread.isConversation = function () {
+    return this.get('type') === 'Conversation';
+  };
+
+  /* Get the public url
+   *
+   * @return {String}
+   */
+  Thread.publicUrl = function () {
+    return '/projects/' + this.get('project_id') + '/' + this.get('type').toLowerCase() + 's/' + this.id;
+  };
+
+  /* Get the API url
+   *
+   * @return {String}
+   */
+  Thread.url = function () {
+    return '/api/1' + this.publicUrl();
+  };
+
+  /* Get the comments url
+   *
+   * @return {String}
+   */
+  Thread.commentsUrl = function () {
+    return this.url() + '/comments';
+  };
+
+  /* Parses response and builds an array of comments attached to the thread
+   *
+   * @param {Response} response
+   * @return {Array}
+   */
+  Thread.parseComments = function (response) {
+    var thread_attributes = response.objects
+      , comment_attributes = _.detect(response.references, function (ref) {
+          return thread_attributes.recent_comment_ids[0] === ref.id;
+        })
+      , assigned_user = _.detect(response.references, function (ref) {
+          return ref.type === 'Person' && comment_attributes.assigned_id === ref.id;
+        })
+      , project = _.detect(response.references, function (ref) {
+          return ref.type === 'Project' && comment_attributes.project_id === ref.id;
+        });
+
+    if (assigned_user) {
+      comment_attributes.assigned = assigned_user.user;
     }
 
-    /* Checks if the thread is a Task
-     *
-     * @return {Boolean}
-     */
-  , isTask: function () {
-      return this.get('type') === 'Task';
+    if (project) {
+      comment_attributes.project = project;
     }
 
-    /* Checks if the thread is a Conversation
-     *
-     * @return {Boolean}
-     */
-  , isConversation: function () {
-      return this.get('type') === 'Conversation';
+    return comment_attributes;
+  };
+
+  Thread.parse = function (response) {
+    if (response.objects) {
+      return _.parseFromAPI(response.objects);
+    } else {
+      return _.parseFromAPI(response)[0];
     }
+  };
 
-  , url: function () {
-      switch (this.get('type')) {
-      case 'Conversation':
-        return '/api/1/projects/' + this.get('project_id') + "/conversations/" + this.id;
-      case 'Task':
-        return '/api/1/projects/' + this.get('project_id') + "/tasks/" + this.id;
-      }
-    }
-
-  , commentsUrl: function () {
-      return this.url() + '/comments';
-    }
-
-    /* Parses response and builds an array of comments attached to the thread
-     *
-     * @param {Response} response
-     * @return {Array}
-     */
-  , parseComments: function (response) {
-      var thread_attributes = response.objects
-        , comment_attributes = _.detect(response.references, function (ref) {
-            return thread_attributes.recent_comment_ids[0] === ref.id;
-          })
-        , assigned_user = _.detect(response.references, function (ref) {
-            return ref.type === 'Person' && comment_attributes.assigned_id === ref.id;
-          })
-        , project = _.detect(response.references, function (ref) {
-            return ref.type === 'Project' && comment_attributes.project_id === ref.id;
-          });
-
-      if (assigned_user) {
-        comment_attributes.assigned = assigned_user.user;
-      }
-
-      if (project) {
-        comment_attributes.project = project;
-      }
-
-      return comment_attributes;
-    }
-
-  , parse: function (response) {
-      if (response.objects) {
-        return _.parseFromAPI(response.objects);
-      } else {
-        return _.parseFromAPI(response)[0];
-      }
-    }
-  });
-
+  // exports
+  Teambox.Models.Thread = Teambox.Models.Base.extend(Thread);
 }());
-
--- app/javascripts/views/search.js
+++ app/javascripts/views/search.js
@@ -1,24 +1,36 @@
-// Displays quick search results when typing on the top bar
+(function  () {
+  var Search = { className: 'search_bar'
+               , template: Teambox.modules.ViewCompiler('partials.search_bar')
+               , quickresult_template: Teambox.modules.ViewCompiler('search.quicksearch')
+               , loading_template: Teambox.modules.ViewCompiler('search.loading')
+               , results_template: Teambox.modules.ViewCompiler('search.results')
+               ,  icons: { Conversation: 'comment_icon'
+                         , TaskList: 'task_icon'
+                         , Task: 'task_icon'
+                         , Page: 'page_icon'
+                         }
+               };
 
-Teambox.Views.Search = Backbone.View.extend({
+  Search.events = {
+    'keydown input#searchbox':  'navigateQuickResults'
+  , 'keyup input#searchbox':    'showQuickResults'
+  , 'focusout input#searchbox': 'hideQuickResults'
+  }
 
-  events: {
-    "keydown input#searchbox":  "navigateQuickResults",
-    "keyup input#searchbox":    "showQuickResults",
-    "focusout input#searchbox": "hideQuickResults"
-  },
+  /* Updates current el
+   *
+   * @return self
+   */
+  Search.render = function () {
+    this.el.update(this.template());
+    return this;
+  };
 
-  quickresult_template: Handlebars.compile(Templates.search.quicksearch),
-  loading_template: Handlebars.compile(Templates.search.loading),
-  results_template: Handlebars.compile(Templates.search.results),
-
-  initialize: function(options) {
-    this.app = options.app;
-  },
-
-  // Keyboard navigation for quick results
-  navigateQuickResults: function(evt) {
-    // When pressing Enter..
+  /* Keyboard navigation for quick results
+   *
+   * @param {Event} evt
+   */
+  Search.navigateQuickResults = function (evt) {
     if (evt.keyCode === Event.KEY_RETURN) {
       // Run full search, and prevent submitting the form
       if (this.highlight_index === 0) {
@@ -40,125 +52,159 @@ Teambox.Views.Search = Backbone.View.extend({
     }
 
     // Prevent up/down cursor actions on the input
-    if (evt.keyCode === Event.KEY_UP) { evt.stop(); return; }
-    if (evt.keyCode === Event.KEY_DOWN) { evt.stop(); return; }
-  },
+    if (evt.keyCode === Event.KEY_UP) {
+      return evt.stop();
+    }
 
-  // Display the 20 most recent matching results
-  showQuickResults: function(evt) {
+    if (evt.keyCode === Event.KEY_DOWN) {
+      return evt.stop();
+    }
+  };
+
+  /* Display the 20 most recent matching results
+   *
+   * @param {Event} evt
+   */
+  Search.showQuickResults = function (evt) {
+    var search_term = $('searchbox').value
+      , regex = RegExp('\\W' + search_term, 'i')
+      , found = Teambox.collections.tasks.models.select(function (el) {
+          return el.get('name') && (' ' + el.get('name').toLowerCase()).match(regex);
+        }).sortBy(function (el) {
+          return el.updated_at;
+        }).slice(0, 20).collect(function (el) {
+          return el.getAttributes();
+        });
+
     // Ignore Enter and Esc, since they are already handled on keydown
-    if (evt.keyCode === Event.KEY_RETURN) { evt.stop(); return; }
-    if (evt.keyCode === Event.KEY_ESC) { evt.stop(); return; }
+    if (evt.keyCode === Event.KEY_RETURN) {
+      return evt.stop();
+    }
+
+    if (evt.keyCode === Event.KEY_ESC) {
+      return evt.stop();
+    }
 
     // Move highlighted element up or down, if possible
-    if (evt.keyCode === Event.KEY_UP) { this.moveHighlight(-1); evt.stop(); return; }
-    if (evt.keyCode === Event.KEY_DOWN) { this.moveHighlight(1); evt.stop(); return; }
+    if (evt.keyCode === Event.KEY_UP) {
+      this.moveHighlight(-1);
+      return evt.stop();
+    }
 
-    var search_term = $('searchbox').value;
+    if (evt.keyCode === Event.KEY_DOWN) {
+      this.moveHighlight(1);
+      return evt.stop();
+    }
 
     // If text search is empty, hide the results box
-    if(search_term.length === 0) {
+    if (search_term.length === 0) {
       this.hideQuickResults();
     }
 
-    // Find matches among the titles of my elements
-    var regex = RegExp('\\W'+search_term, 'i');
-    var found = this.app.my_tasks.models.select(function(i) {
-      return i.get('name') &&
-        (" "+i.get('name').toLowerCase()).match(regex);
-    }).sortBy(function(i) {
-      return i.updated_at;
-    }).slice(0,20).collect(function(i) { return i.getAttributes(); });
-
     // Display the dropdown menu with results
     $$('#quicksearch_results').invoke('remove');
-    $(document.body).insert({ top:
-      this.quickresult_template({ results: found, query: escape(search_term) })
-    });
+    $(document.body).insert({top: this.quickresult_template({results: found, query: search_term})});
     this.highlight_index = 0;
 
     // Highlight matches in results
-    $$('#quicksearch_results li a').each(function(a) {
+    $$('#quicksearch_results li a').each(function (a) {
       var regex = new RegExp(search_term, 'ig');
       a.innerHTML = a.innerHTML.replace(regex, "<b>$&</b>");
     });
-  },
+  };
 
-  // Move the highlighted index in quick results up and down
-  moveHighlight: function(inc) {
-    if (!$('quicksearch_results')) { return; }
-    this.highlight_index = (this.highlight_index + inc) || 0;
+  /* Move the highlighted index in quick results up and down
+   *
+   * @param {Integer} inc
+   */
+  Search.moveHighlight = function (inc) {
     var lis = $('quicksearch_results').select('li');
+
+    if (!$('quicksearch_results')) {
+      return;
+    }
+
+    this.highlight_index = (this.highlight_index + inc) || 0;
     this.highlight_index = [0, this.highlight_index].max();
-    this.highlight_index = [lis.length-1, this.highlight_index].min();
+    this.highlight_index = [lis.length - 1, this.highlight_index].min();
+
     lis.invoke('removeClassName', 'selected');
     lis[this.highlight_index].addClassName('selected');
-  },
+  };
 
-  // Fade out the quick results dialog
-  hideQuickResults: function() {
+  /* Fade out the quick results dialog
+   */
+  Search.hideQuickResults = function () {
     $$('#quicksearch_results').invoke('fade');
-  },
+  };
 
-  // Focus on the search box and select all
-  focus: function() {
+  /* Focus on the search box and select all
+   */
+  Search.focus = function () {
     $('searchbox').focus();
     $('searchbox').select();
-  },
+  };
 
-  // Send the search query through the controller, and fetch results
-  submitSearch: function() {
+  /* Send the search query through the controller, and fetch results
+   */
+  Search.submitSearch = function () {
     this.hideQuickResults();
-    document.location.hash = "#!/search/"+escape($('searchbox').value);
-  },
+    document.location.hash = "#!/search/" + escape($('searchbox').value);
+  };
 
-  // Perform an API call to fetch full search results in Sphinx
-  // TODO: Should we move this into a collection?
-  getResults: function(query) {
+  /* Perform an API call to fetch full search results in Sphinx
+   * TODO: Should we move this into a collection?
+   *
+   * @param {String} query
+   */
+  Search.getResults = function (query) {
     // Populate searchbox if it's empty (because we loaded a search URL)
-    if(!$('searchbox').value) { $('searchbox').value = query; }
+    if (!$('searchbox').value) {
+      $('searchbox').value = query;
+    }
 
-    var self = this;
-    var url = "/api/1/search/?q="+escape(query);
-    var req = new Ajax.Request(url, {
-      method: "get",
-      onLoading: function(r) {
+    var self = this
+      , url = "/api/1/search/?q=" + escape(query);
+
+    new Ajax.Request(url, {
+      method: 'get'
+    , onLoading: function (r) {
         // Display a placeholder for search
         $('content').update(
           self.loading_template({ query: query })
         );
         $('content').addClassName('search_results');
-      },
-      onComplete: function(r) {
+      }
+    , onComplete: function (r) {
         $('content').down('.loading').remove();
-      },
-      onSuccess: function(r) {
+      }
+    , onSuccess: function (r) {
         var results = _.parseFromAPI(JSON.parse(r.responseText));
         self.displayResults(results);
-      },
-      onFailure: function(r) {
+      }
+    , onFailure: function (r) {
         $('search_result').update('<p>An error occurred, please try reloading the page.</p>');
       }
     });
-  },
+  };
 
-  // Render full search results
-  displayResults: function(results) {
+  /* Render full search results
+   *
+   * @param {Array} results
+   */
+  Search.displayResults = function (results) {
     var self = this;
-    results.each(function(r) {
+
+    results.each(function (r) {
       r.icon_class = self.icons[r.type];
       r.name = (r.name || r.first_comment.stripTags()).truncate(65);
     });
+
     $('search_results').update(
-      this.results_template({ results: results, length: results.length })
+      this.results_template({results: results, length: results.length})
     );
-  },
+  };
 
-  icons: {
-    "Conversation": 'comment_icon',
-    "TaskList": 'task_icon',
-    "Task": 'task_icon',
-    "Page": 'page_icon'
-  }
-
-});
+  // exports
+  Teambox.Views.Search = Backbone.View.extend(Search);
+}());
