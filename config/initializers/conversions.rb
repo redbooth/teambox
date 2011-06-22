f38dec5d72aef1aea501954acb238e8d7415fe68
Enric Lluelles <enriclluelles@gmail.com>
2011-06-22 15:41:55 +0200

    Using I18n.l for date formats instead of to_s

--- config/initializers/conversions.rb
+++ config/initializers/conversions.rb
@@ -1,4 +1,2 @@
-Time::DATE_FORMATS[:month_and_year] = "%B %Y"
-Time::DATE_FORMATS[:month_and_day] = "%B %d"
 Time::DATE_FORMATS[:api_time] = "%Y-%m-%d %H:%M:%S %z"
 Time::DATE_FORMATS[:csv_time] = "%m/%d/%Y %H:%M"   
