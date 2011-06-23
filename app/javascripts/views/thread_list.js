0a9468c84deec07803d8be2cc51b0a22ec019d0c
James Urquhart <jamesu@gmail.com>
2011-06-23 17:31:20 +0100

    ThreadList now updates when the collection updates

--- app/javascripts/views/thread_list.js
+++ app/javascripts/views/thread_list.js
@@ -9,17 +9,31 @@
 
   ThreadList.initialize = function (options) {
     _.bindAll(this, "render");
+    this.collection.bind('add', this.addThread);
+    this.collection.bind('remove', this.removeThread);
+    this.collection.bind('refresh', this.reload);
+  };
+
+  ThreadList.addThread = function(thread, collection) {
+    this.el.insert({top: new Teambox.Views.Thread({model:thread}).render().el});
+  };
+
+  ThreadList.removeThread = function(thread, collection) {
+    this.el.find('.thread[data-class=conversation, data-id='+thread.id+', data-project-id='+thread.get('project_id')+']').remove();
+  };
+
+  ThreadList.reload = function(collection) {
+    var self = this;
+    collection.each(function(model){
+      var view = new Teambox.Views.Thread({model:model});
+      self.el.insert({bottom: view.render().el});
+    });
   };
 
   ThreadList.render = function () {
     var Views = Teambox.Views, self = this;
     this.el.update('');
-
-    this.collection.each(function(model){
-      var view = new Views.Thread({model:model});
-      self.el.insert({bottom: view.render().el});
-    });
-
+    this.reload(this.collection);
     return this;
   };
 
