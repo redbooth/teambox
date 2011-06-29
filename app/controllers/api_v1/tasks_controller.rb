6ee75728d02d76227d28b8795c57cf4d3fdc1cbb
Saimon Moore <saimon@saimonmoore.net>
2011-06-29 12:26:44 +0200

    Using references => true shortcut and including uploads in api conv/tasks controller #update

--- app/controllers/api_v1/tasks_controller.rb
+++ app/controllers/api_v1/tasks_controller.rb
@@ -44,7 +44,7 @@ class ApiV1::TasksController < ApiV1::APIController
     @task.updating_user = current_user
 
     if @task.update_attributes(params)
-      handle_api_success(@task, :wrap_objects => true, :references => [:comments], :include => [:uploads])
+      handle_api_success(@task, :wrap_objects => true, :references => true, :include => [:user, :uploads])
     else
       handle_api_error(@task)
     end
