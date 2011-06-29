5432213566d33afebd2b155f83ba11fa9386e199
Saimon Moore <saimon@saimonmoore.net>
2011-06-29 12:33:10 +0200

    Correct handling of api responses after latest synch with dev

--- app/javascripts/views/upload_area.js
+++ app/javascripts/views/upload_area.js
@@ -80,7 +80,7 @@
         , response = JSON.parse(iframe_body.firstChild.innerHTML);
 
       if (iframe_body.className !== "error") {
-        self.model.set(response.objects);
+        self.model.set(response.objects ? response.objects : response);
         self.comment_form.addComment(false, response, true);
       } else {
         self.comment_form.handleError(false, response);
