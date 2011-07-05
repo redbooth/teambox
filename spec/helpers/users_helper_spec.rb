959ff58502fb824563ab297968e2d35d0b2a9675
Jordi Romero <jordi@jrom.net>
2011-07-05 15:01:26 +0200

    fix json_organizations to not include deleted orgs

--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -4,11 +4,11 @@ describe UsersHelper do
   before do
     @user = Factory(:confirmed_user)
     @project = Factory(:project, :user => @user)
+    login_as @user
   end
 
   describe "json_projects" do
     before do
-      login_as @user
       @projects = JSON.parse(json_projects)
     end
     it "should return an hash of projects with the user's role" do
@@ -16,6 +16,34 @@ describe UsersHelper do
       project['name'].should == @project.name
       project['permalink'].should == @project.permalink
       project['role'].should == Person::ROLES[:admin].to_s
+    end
+  end
+
+  describe "json_organizations" do
+    it "shouldn't include deleted organizations" do
+      o1 = Factory :organization
+      o2 = Factory :organization
+      o1.add_member @user
+      o2.add_member @user
+      o2.projects.destroy_all
+      o2.destroy.should be_true
+      organizations = JSON.parse(json_organizations)
+      organizations.collect {|o| o['permalink']}.should == [@project.organization.permalink, o1.permalink]
+    end
+  end
+
+  describe "json_external_organizations" do
+    it "shouldn't include deleted organizations" do
+      p1 = Factory :project
+      o1 = p1.organization
+      p2 = Factory :project
+      o2 = p2.organization
+      p1.add_user @user
+      p2.add_user @user
+      o2.projects.destroy_all
+      o2.destroy.should be_true
+      organizations = JSON.parse(json_external_organizations)
+      organizations.collect {|o| o['permalink']}.should == [@project.organization.permalink, o1.permalink]
     end
   end
 end
