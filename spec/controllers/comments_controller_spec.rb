ed167b0f243710cc0544a3777965e6226d27f328
James Urquhart <jamesu@gmail.com>
2011-07-11 13:05:46 +0100

    Better handling of the comment rollback on destruction feature

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -22,4 +22,30 @@ describe CommentsController do
       Comment.last.user.should == @user
     end
   end
+  
+  describe "#destroy rollback" do
+    before do
+      task_comment_rollback_example(@project)
+    end
+    
+    it "reverts the status back to the previous status when destroyed as the last comment with do_rollback" do
+      login_as @project.user
+      put :destroy, :project_id => @project.permalink, :task_id => @task.id, :id => @task.comments.last.id
+      
+      @task = Task.find_by_id(@task.id)
+      @task.due_on.should == @old_time
+      @task.assigned_id.should == @old_assigned_id
+      @task.status.should == @old_status
+    end
+    
+    it "maintains the status if any other comments are destroyed" do
+      login_as @project.user
+      put :destroy, :project_id => @project.permalink, :task_id => @task.id, :id => @task.comments[1].id
+      
+      @task = Task.find_by_id(@task.id)
+      @task.due_on.should == @new_time
+      @task.status.should == @new_status
+      @task.assigned_id.should == @new_assigned_id
+    end
+  end
 end
