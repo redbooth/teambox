418cefb09bc1aee7253c8ee162fa0a92f2ef95b2
Pablo Villalba <pablo@teambox.com>
2011-07-31 00:46:03 +0200

    Alert before leaving the page with unsaved data

--- app/javascripts/lib/backbone.paranoid.js
+++ app/javascripts/lib/backbone.paranoid.js
@@ -2,20 +2,37 @@
 // before navigating away with Backbone. If there is a dirty textarea,
 // we will ask for confirmation to navigate away
 
+// Idea: We could also make forms dirty when their elements are edited
+// and unset the dirty attribute on ajax:success for the form
+
+window.isPageDirty = function() {
+  return $$("#content textarea").detect(function(textarea) {
+    return (textarea.value || "").length > 0;
+  });
+}
+
+window.onbeforeunload = function(e) {
+  e = e || window.event;  
+  if (window.isPageDirty()) {
+    // For IE and Firefox
+    if (e) { e.returnValue = "You have unsaved changes."; }
+    // For Webkit
+    return "You have unsaved changes.";
+  }
+};
+
+// Patch Backbone to prevent navigating away a dirty form
+
 Backbone.History.prototype.super_checkUrl = Backbone.History.prototype.checkUrl;
 Backbone.History.prototype.checkUrl = function() {
 
   // We're adding a filter before changing the URL
   var unsaved_changes =
     !this.skipUnsavedCheck &&
-    $$("#content textarea").detect(
-      function(textarea) {
-        return (textarea.value || "").length > 0;
-      });
+    window.isPageDirty();
 
   if (unsaved_changes) {
     if(!confirm("There are unsaved changes on this page.\n\nAre you sure you want to navigate away?")) {
-      console.log("Aborting navigation");
       window.location.hash = this.fragment;
       // Since we are changing the hash, don't ask to confirm navigation next time
       this.skipUnsavedCheck = true;
