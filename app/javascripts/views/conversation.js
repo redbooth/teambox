e5518c25b6cb3134c6f39e55c4646fcd41f0a212
Jack Bach <jackjackbach@gmail.com>
2011-07-31 13:01:10 +0200

    option 1 for loading comments in conversations

--- app/javascripts/views/conversation.js
+++ app/javascripts/views/conversation.js
@@ -1,6 +1,7 @@
 (function () {
   var Conversation = { template: Teambox.modules.ViewCompiler('conversations.show')
                      , loading: Teambox.modules.ViewCompiler('partials.loading')
+                     , comment_template: Teambox.modules.ViewCompiler('partials.comment')
                      };
 
   Conversation.initialize = function (options) {
@@ -13,6 +14,7 @@
   /* TODO: Handle 404s or permission denied for conversations
    * TODO: Shouldn't render to the DOM!
    */
+/*
   Conversation.render = function () {
     if(this.model.isLoaded()) {
       var html = this.template(this.model.getAttributes());
@@ -26,7 +28,41 @@
     }
     return this;
   }
+*/
+  Conversation.render = function () {
+    if(this.model.isLoaded()) {
+      var self = this;
+      
+      var html = this.template(this.model.getAttributes());
+      this.el.update(html);
+      
+      var options = {
+        conversation_id: this.model.id
+      };
+      
+      comments = new Teambox.Collections.Comments([],options);
+      
+      // Show loader
+      this.el.down('.comments').update("<img src='/images/loading.gif' alt='Loading' />");
+      
+      
+      comments.fetch({
+        success: function (collection) {
+          var html = '';
+          _.each(collection.models.reverse(), function (model) {
+            html += self.comment_template(model.attributes);
+          });
+          self.el.down('.comments').update(html);
+        }
+      });
+      
 
+      
+    } else {
+      this.el.update(loading());
+    }
+    return this;
+  };
   // exports
   Teambox.Views.Conversation = Backbone.View.extend(Conversation);
 }());
