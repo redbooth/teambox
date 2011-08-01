84a2797a947948e373c087ac43e1320d4303326d
Pablo Villalba <pablo@teambox.com>
2011-08-01 21:40:02 +0200

    Show ajax_loading_spinner for jQuery requests too

--- app/javascripts/modules/ajax_loading_spinner.js
+++ app/javascripts/modules/ajax_loading_spinner.js
@@ -2,8 +2,26 @@
 
 Ajax.Responders.register({
   onCreate:   function() {
+    window.activeRequests = window.activeRequests || 0;
+    window.activeRequests++;
     $("global_loading").show(); },
   onComplete: function() {
-    if (Ajax.activeRequestCount === 0) {
+    window.activeRequests--;
+    if (window.activeRequests === 0) {
       $("global_loading").hide(); } }
+});
+
+// Support for jQuery, until we remove the dependencies on it
+jQuery(document).ajaxStart(function() {
+  window.activeRequests = window.activeRequests || 0;
+  // Prevent going below 0 (in case some requests are sent before this
+  // code starts tracking them)
+  window.activeRequests = Math.max(window.activeRequests + 1, 0);
+  $("global_loading").show();
+});
+
+jQuery(document).ajaxComplete(function() {
+  window.activeRequests = Math.max(window.activeRequests - 1, 0);
+  if (window.activeRequests === 0) {
+    $("global_loading").hide(); }
 });
