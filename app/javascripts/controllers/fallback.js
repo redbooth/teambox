e4a3d99938bc491fff015e1a04de943e860ce593
Charles Barbier <unixcharles@gmail.com>
2011-08-10 13:08:25 -0400

    Finish fallback controller   - Update body class   - extract title   - Handle forms in the fallback controller   - Highlight sidebar menu sooner for snappier feeling

--- app/javascripts/controllers/fallback.js
+++ app/javascripts/controllers/fallback.js
@@ -1,36 +1,42 @@
 (function () {
+  /* Extract text/html response from the parts.haml
+   * layout and build a parts object */
+  var extractParts = function(responseText) {
+    if (!$('_extractor')) {
+      $(document.body).insert({ bottom:
+        "<div id='_extractor' style='display:none'></div>" });
+    }
+    var extractor = $('_extractor');
+    extractor.insert({ top: responseText });
+    var response = { body_classes: extractor.down(".body_classes").innerHTML
+      , title: extractor.down(".title_part")
+      , content: extractor.down(".content_part")
+      , column: extractor.down(".column_part")
+    };
+    return response;
+  };
+
+
   Teambox.Controllers.FallbackController = {
     show: function() {
+      var current = Teambox.Views.Sidebar.detectSelectedSection(window.location.hash);
+      Teambox.Views.Sidebar.highlightSidebar(current)
+
       var fragment = Backbone.history.fragment;
 
       new Ajax.Request(Backbone.history.fragment+"?extractparts=1", {
         method: "get",
         onSuccess: function(r) {
-          var extractParts = function(responseText) {
-            if (!$('_extractor')) {
-              $(document.body).insert({ bottom:
-                "<div id='_extractor' style='display:none'></div>" });
-            }
-            var extractor = $('_extractor');
-            extractor.insert({ top: responseText });
-            var response = {
-              body_classes: extractor.down(".body_classes").innerHTML,
-              content: extractor.down(".content_part"),
-              column: extractor.down(".column_part")
-            };
-            return response;
-          };
-
-          var current = Teambox.Views.Sidebar.detectSelectedSection(window.location.hash);
-          Teambox.Views.Sidebar.highlightSidebar(current)
-
           // Insert the content, and update posted dates if necessary
           var parts = extractParts(r.responseText)
-          if (!parts.content)
-            return this.onFailure(r);
 
           document.body.className = parts.body_classes;
           $('content').update(parts.content);
+          $('content').addClassName('ajax_forms');
+          //$('column').update(parts.column);
+          $('view_title').update(parts.title);
+
+
           Date.format_posted_dates();
           Task.insertAssignableUsers();
           disableConversationHttpMethodField();
@@ -52,8 +58,18 @@
     if (proceed() == false) {
       Teambox.Controllers.FallbackController.show();
       var matched = true;
+    } else {
+      // Clear the body classes to avoid forms to act like ajax form
+      $('content').removeClassName('ajax_forms');
     }
     return matched;
+  })
+
+  /* When a remote form is submitted in an ajax page, extract the response
+   * and update the content div */
+  document.on('ajax:complete', '.ajax_forms form', function(e, form) {
+    var parts = extractParts(e.memo.responseText);
+    $('content').update(parts.content);
   })
 
 }());
