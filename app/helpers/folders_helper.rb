230c6a0b72165559b6f72a6cd31ec677bb916b5f
Artur Szwed <artur1979@googlemail.com>
2011-08-10 15:42:44 +0200

    Fixed ajax browsing in folders breadcrumb

--- app/helpers/folders_helper.rb
+++ app/helpers/folders_helper.rb
@@ -4,7 +4,7 @@ module FoldersHelper
     folder = @current_folder
     bc = []
     until folder.nil? do
-      bc << (folder == @current_folder ? folder.name : link_to(folder.name, project_folder_path(@current_project, folder)))
+      bc << (folder == @current_folder ? folder.name : link_to(folder.name, project_folder_path(@current_project, folder), :remote => true))
       folder = folder.parent_folder
     end
     bc << link_to(@current_project, project_uploads_path(@current_project), :remote => true)
