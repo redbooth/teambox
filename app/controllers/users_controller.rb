e78212a8daf85e38da04695703f1a8bdf094eb8f
Charles Barbier <unixcharles@gmail.com>
2011-08-10 13:17:47 -0400

    Change user#edit to use the default layout behaviour to make it work with fallback controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -114,8 +114,6 @@ class UsersController < ApplicationController
     else
       render :file => "#{Rails.root}/public/404.html", :status => 404
     end
-
-    render :layout => 'settings'
   end
 
   def update
