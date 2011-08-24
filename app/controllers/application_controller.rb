49399961263462a8319669b0ae4d96bedd0b768a
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-08-24 18:18:11 +0200

    Magic comment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+# -*- encoding: utf-8 -*-
+
 # Filters added to this controller apply to all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
 
