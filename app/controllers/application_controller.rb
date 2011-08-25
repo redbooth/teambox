b33f8860ec6d843810d38b187ae1909c0a8b266d
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-08-25 12:26:26 +0200

    Removed trailing whitespace

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
-  
+
   include AuthenticatedSystem
   include SentientController
   include PushSession
