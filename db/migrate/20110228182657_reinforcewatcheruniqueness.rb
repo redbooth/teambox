b7125303cc7d66496606f31a0c86a63d3b28de14
Charles Barbier <unixcharles@gmail.com>
2011-08-26 15:07:07 -0400

    add ActiveRecord alternative for non-mysql database to watcher uniqueness index

--- db/migrate/20110228182657_reinforcewatcheruniqueness.rb
+++ db/migrate/20110228182657_reinforcewatcheruniqueness.rb
@@ -4,15 +4,26 @@ class Reinforcewatcheruniqueness < ActiveRecord::Migration
       remove_index :watchers, {:name => 'watchers_uniqueness_index'}
     end
 
-    Watcher.connection.execute <<-EOF
-      DELETE #{Watcher.table_name}
-      FROM #{Watcher.table_name},
-        (SELECT MAX(id) as dupid, COUNT(id) as dupcnt, user_id, watchable_id, watchable_type
-         FROM #{Watcher.table_name}
-         GROUP BY user_id, watchable_id, watchable_type
-         HAVING dupcnt > 1) as duplicates
-      WHERE #{Watcher.table_name}.id = duplicates.dupid
-    EOF
+    if ActiveRecord::Base.configurations[Rails.env]['adapter'] =~ /(mysql)/i
+      Watcher.connection.execute <<-EOF
+        DELETE #{Watcher.table_name}
+        FROM #{Watcher.table_name},
+          (SELECT MAX(id) as dupid, COUNT(id) as dupcnt, user_id, watchable_id, watchable_type
+           FROM #{Watcher.table_name}
+           GROUP BY user_id, watchable_id, watchable_type
+           HAVING dupcnt > 1) as duplicates
+        WHERE #{Watcher.table_name}.id = duplicates.dupid
+      EOF
+    else
+      Watcher.find_each do |watch_tag|
+        conditions = { :user_id => watch_tag.user_id, :watchable_id => watch_tag.watchable_id, :watchable_type => watch_tag.watchable_type }
+        unless Watcher.where(conditions).count == 1
+          watchers = Watcher.where(conditions)
+          duplicated = watchers.where(:id => watch_tag.id)
+          duplicated.delete_all
+        end
+      end
+    end
 
     unless index_exists?(:watchers, [:user_id, :watchable_id, :watchable_type], :name => 'watchers_uniqueness_index', :unique => true)
       add_index :watchers, [:user_id, :watchable_id, :watchable_type], :name => 'watchers_uniqueness_index', :unique => true
