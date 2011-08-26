14b7c3373da4995a0e0228e969cacf91be43f4ec
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-08-26 12:57:47 +0200

    For the moment do not store anything in Redis

--- vendor/plugins/teambox_api_v2/app/controllers/api_v2/conversations_controller.rb
+++ vendor/plugins/teambox_api_v2/app/controllers/api_v2/conversations_controller.rb
@@ -11,31 +11,16 @@ class ApiV2::ConversationsController < ApiV2::BaseController
   #
   def index
     authorize!(:show, context)
-
-    if (value = $redis.get(redis_key))
-      logger.info "[Redis] get(#{redis_key})"
-      json = value
-    else
-      @conversations = conversation_context.
-                       except(:order).
-                       where(api_range('conversations')).
-                       where(['is_private = ? OR (is_private = ? AND watchers.user_id = ?)', false, true, current_user.id]).
-                       joins("LEFT JOIN watchers ON (conversations.id = watchers.watchable_id AND watchers.watchable_type = 'Conversation') AND watchers.user_id = #{current_user.id}").
-                       limit(api_limit).
-                       order('conversations.id DESC')
-      json = render_to_string
-      $redis.set(redis_key, json)
-      logger.info "[Redis] set(#{redis_key})"
-    end
-
-    render :text => json
+    @conversations = conversation_context.
+                     except(:order).
+                     where(api_range('conversations')).
+                     where(['is_private = ? OR (is_private = ? AND watchers.user_id = ?)', false, true, current_user.id]).
+                     joins("LEFT JOIN watchers ON (conversations.id = watchers.watchable_id AND watchers.watchable_type = 'Conversation') AND watchers.user_id = #{current_user.id}").
+                     limit(api_limit).
+                     order('conversations.id DESC')
   end
 
   private
-
-  def redis_key
-    request.path.parameterize
-  end
 
   def context
     @current_project || current_user
