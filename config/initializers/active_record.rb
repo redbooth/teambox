1d43113a58664bf751104d2e02786c85012653a3
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-08-31 11:58:06 +0200

    Missing methods for ActiveRecord::Relation

--- config/initializers/active_record.rb
+++ config/initializers/active_record.rb
@@ -4,6 +4,14 @@ ActiveRecord::Relation.class_eval do
   def new_by_user(user, attributes = {})
     new(attributes) { |obj| obj.user = user; yield(obj) if block_given? }
   end
+
+  def build_by_user(user, attributes = {})
+    build(attributes) { |obj| obj.user = user; yield(obj) if block_given? }
+  end
+
+  def create_by_user(user, attributes = {})
+    create(attributes) { |obj| obj.user = user; yield(obj) if block_given? }
+  end
 end
 
 ActiveRecord::Associations::AssociationCollection.class_eval do
