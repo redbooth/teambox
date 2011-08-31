8e51d7e3b9e4332fd14c826d53ecb65fe68abc89
Artur Szwed <artur1979@googlemail.com>
2011-08-31 11:52:25 +0200

    Added spec for public downloads folders

--- spec/controllers/public_downloads_controller_spec.rb
+++ spec/controllers/public_downloads_controller_spec.rb
@@ -8,6 +8,8 @@ describe PublicDownloadsController do
     @upload = @project.uploads.new({:asset => mock_uploader('semicolons.js', 'application/javascript', "alert('what?!')")})
     @upload.user = @user
     @upload.save!
+
+    @folder = Factory(:folder, :user => @user, :project => @project)
   end
   
   route_matches("/d/aaBBccdd33ffGG44",
@@ -15,6 +17,12 @@ describe PublicDownloadsController do
     :controller => "public_downloads",
     :action => "download", 
     :token => "aaBBccdd33ffGG44")
+
+  route_matches("/f/hhyy66GGJJeeXXll",
+    :get,
+    :controller => "public_downloads",
+    :action => "folder",
+    :token => "hhyy66GGJJeeXXll")
 
   route_matches("/send/aaBBccdd33ffGG44",
     :get,
@@ -31,6 +39,20 @@ describe PublicDownloadsController do
     
     it "should show file not found page for invalid token" do
       get :download, :token => 'th1SiS1nVaL1Dt0k33n'
+      response.code.should eq("404")
+      response.should render_template("public_downloads/not_found")
+    end
+  end
+
+  describe "GET #folder" do
+    it "should display download folder landing page for correct token" do
+      get :folder, :token => @folder.token
+      response.code.should eq("200")
+      response.should render_template("public_downloads/folder")
+    end
+
+    it "should show file not found page for invalid token" do
+      get :folder, :token => 'th1SiS1nVaL1Dt0k33n'
       response.code.should eq("404")
       response.should render_template("public_downloads/not_found")
     end
